cab51d70cedec7cdff1ab4c5fee48e783943fe39
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2008-12-14 16:36:46 +0000

    some more updates. just adding common functions and generally making sure the two trees (webworks-webme and our internal copy) come closer together

--- trunk/common.php
+++ trunk/common.php
@@ -1,4 +1,26 @@
 <?php
+function dbAll($query,$key='') {
+	global $db;
+	$q = $db->query($query);
+	if(PEAR::isError($q)){
+		echo $q->getMessage();
+		exit;
+	}
+	$results=$q->fetchAll();
+	if(!$key)return $results;
+	$arr=array();
+	foreach($results as $r)$arr[$r[$key]]=$r;
+	return $arr;
+}
+function dbRow($query) {
+	global $db;
+	$q = $db->query($query);
+	return $q->fetchRow();
+}
+function dbOne($query, $field='') {
+	$r = dbRow($query);
+	return $r[$field];
+}
 function redirect($addr){
 	echo '<html><head><script type="text/javascript">setTimeout(function(){document.location="'.$addr.'";},10);</script></head><body><noscript>you need javascript to use this site</noscript></body></html>';
 	exit;
@@ -13,10 +35,13 @@ if (!file_exists(BASEDIR . '.private/config.php')) {
 	exit;
 }
 require BASEDIR . '.private/config.php';
+define('FCKEDITOR','fckeditor-2.6.3');
+define('WORKDIR_IMAGERESIZES', BASEDIR.'f/.files/image_resizes/');
+define('WORKURL_IMAGERESIZES', '/f/.files/image_resizes/');
 // }
 // { connect to database
 require_once 'MDB2.php';
-$dsn='mysql://'.$DBVARS['username'].':'.$DBVARS['password'].'@'.$DBVARS['hostname'].'/'.$DBVARS['db_name'];
+$dsn = 'mysql://' . $DBVARS['username'] . ':' . $DBVARS['password'] . '@' . $DBVARS['hostname'] . '/' . $DBVARS['db_name'];
 $db = &MDB2::connect($dsn);
 if(Pear::isError($db)){
 	echo '<p>Error connecting to database.</p><p>Please make sure the access details are correct, and that the server has the Pear packages MDB2 and MDB2_Driver_mysql installed.</p>';
--- trunk/index.php
+++ trunk/index.php
@@ -1,7 +1,6 @@
 <?php
-$time_start=microtime(true);
 // { common variables and functions
 include_once('common.php');
-if(!isset($DBVARS['version']) || $DBVARS['version']<1)redirect('upgrades/upgrade.php');
+if(!isset($DBVARS['version']) || $DBVARS['version']<2)redirect('upgrades/upgrade.php');
 // }
 echo 'ok... tune in for the next exciting episode!';
