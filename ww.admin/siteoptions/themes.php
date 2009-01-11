84a37e155e87860689a7976ed1cf3bae4757f15f
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-01-11 13:07:31 +0000

    fix some missing files

---
+++ ww.admin/siteoptions/themes.php
@@ -0,0 +1,28 @@
+<?php
+echo '<h2>'.__('Themes').'</h2>';
+// { handle actions
+if($action=='set_theme'){
+	if(is_dir(BASEDIR . 'ww.skins/' . $_REQUEST['theme'])){
+		$DBVARS['theme']=$_REQUEST['theme'];
+		$_SESSION['viewing_skin']=$DBVARS['theme'];
+		config_rewrite();
+	}
+}
+// }
+// { samples
+	$dir=new DirectoryIterator(BASEDIR . 'ww.skins');
+	$themes_found=0;
+	foreach($dir as $file){
+		if(strpos($file,'.')===0)continue;
+		$themes_found++;
+		echo '<div style="width:250px;text-align:center;border:1px solid #000;margin:5px;height:200px;float:left;';
+		if($file==$DBVARS['theme'])echo 'background:#ff0;';
+		echo '"><a href="siteoptions.php?page=themes&amp;action=set_theme&amp;theme='.htmlspecialchars($file).'">';
+		if(file_exists(BASEDIR . 'ww.skins/' . $file . '/screenshot.png'))echo '<img src="/ww.skins/'.htmlspecialchars($file).'/screenshot.png" />';
+		echo htmlspecialchars($file);
+		echo '</a></div>';
+	}
+	if($themes_found==0){
+		echo '<em>No themes found. Download a theme and unzip it into the /ww.skins/ directory.</em>';
+	}
+// }
