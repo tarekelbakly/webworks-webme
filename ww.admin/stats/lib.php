f9009056c99d47c1214633db1a5b50b2224683cd
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-02-15 11:05:25 +0000

    make stats work

--- ww.admin/stats/lib.php
+++ ww.admin/stats/lib.php
@@ -3,6 +3,10 @@ function update_stats(){
 	$f=file(USERBASE.'log.txt');
 	foreach($f as $l){
 		list($tmp,$type_data,$user_agent,$referer,$ram_used,$bandwidth,$time_to_render,$db_calls)=explode("	",$l);
+		$ram_used=(int)$ram_used;
+		$bandwidth=(int)$bandwidth;
+		$time_to_render=(float)$time_to_render;
+		$db_calls=(int)$db_calls;
 		$bits=explode(' ',$tmp);
 		list($log_date,$log_type,$ip_address)=array($bits[0].' '.$bits[1],$bits[2],$bits[4]);
 		dbQuery("insert into logs values('$log_date','$log_type','$ip_address','".addslashes($type_data)."','".addslashes($user_agent)."','".addslashes($referer)."',$ram_used,$bandwidth,$time_to_render,$db_calls)");
--- ww.admin/stats/summary.php
+++ ww.admin/stats/summary.php
@@ -3,13 +3,24 @@ update_stats();
 echo '<!--[if IE]><script language="javascript" type="text/javascript" src="/j/flot-0.5/excanvas.pack.js"></script><![endif]-->';
 echo '<script type="text/javascript" src="/j/flot-0.5/jquery.flot.pack.js"></script>';
 
-echo '<div id="placeholder" style="width:600px;height:300px;"></div><p>Use the bar below to select a range</p><div id="overview" style="margin-left:50px;margin-top:20px;width:400px;height:50px"></div>';
+echo '<table><tr><td>visitors</td><td><div id="placeholder" style="width:600px;height:300px;"></div></td><td>page requests</td></tr></table>';
+echo '<p>Use the bar below to select a range</p><div id="overview" style="margin-left:50px;margin-top:20px;width:400px;height:50px"></div>';
 
-$visitors=dbAll('select unix_timestamp(date(log_date)) as log_d,count(log_date) as visitors from logs group by log_d order by log_date');
+$requests_per_days=dbAll('select unix_timestamp(date(log_date)) as log_d,count(log_date) as page_requests from logs group by log_d order by log_date');
+$unique_visitors=dbAll('select unix_timestamp(log_d2) as log_d,count(log_d2) as visitors from (select distinct date(log_date) as log_d2,ip_address from logs) as d1 group by log_d order by log_d');
+
 echo '<script type="text/javascript">window.visitors=[';
-foreach($visitors as $visitor){
+$max_visitors=0;
+foreach($unique_visitors as $visitor){
 	echo '['.$visitor['log_d'].'000,'.$visitor['visitors'].'],';
+	if($max_visitors<$visitor['visitors'])$max_visitors=$visitor['visitors'];
 }
-echo '];</script>';
+echo '];window.page_requests=[';
+$max_page_requests=0;
+foreach($requests_per_days as $requests_per_day){
+	echo '['.$requests_per_day['log_d'].'000,'.$requests_per_day['page_requests'].'],';
+	if($max_page_requests<$requests_per_day['page_requests'])$max_page_requests=$requests_per_day['page_requests'];
+}
+echo "];window.max_visitors=$max_visitors;window.max_page_requests=$max_page_requests;</script>";
 
 echo '<script type="text/javascript" src="stats/summary.js"></script>';
