6dbafa68dd3940ba9a322746cbc69f0b3848a682
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-03-21 17:14:40 +0000

    allow theme variants.

--- ww.admin/siteoptions/themes.php
+++ ww.admin/siteoptions/themes.php
@@ -4,6 +4,7 @@ echo '<h2>'.__('Themes').'</h2>';
 if($action=='set_theme'){
 	if(is_dir(THEME_DIR.'/'.$_REQUEST['theme'])){
 		$DBVARS['theme']=$_REQUEST['theme'];
+		$DBVARS['theme_variant']=@$_REQUEST['theme_variant'];
 		config_rewrite();
 	}
 }
@@ -12,15 +13,27 @@ if($action=='set_theme'){
 	$dir=new DirectoryIterator(THEME_DIR);
 	$themes_found=0;
 	foreach($dir as $file){
-		if(strpos($file,'.')===0)continue;
+		if($file->isDot())continue;
 		if(!file_exists(THEME_DIR.'/'.$file.'/screenshot.png'))continue;
 		$themes_found++;
-		echo '<div style="width:250px;text-align:center;border:1px solid #000;margin:5px;height:200px;float:left;';
+		echo '<div style="width:250px;text-align:center;border:1px solid #000;margin:5px;height:250px;float:left;';
 		if($file==$DBVARS['theme'])echo 'background:#ff0;';
-		echo '"><a href="siteoptions.php?page=themes&amp;action=set_theme&amp;theme='.htmlspecialchars($file).'">';
+		echo '"><form method="post" action="siteoptions.php"><input type="hidden" name="page" value="themes" /><input type="hidden" name="action" value="set_theme" />';
+		echo '<input type="hidden" name="theme" value="'.htmlspecialchars($file).'" />';
 		echo '<img src="/ww.skins/'.htmlspecialchars($file).'/screenshot.png" />';
-		echo htmlspecialchars($file);
-		echo '</a></div>';
+		echo '<strong>',htmlspecialchars($file),'</strong><br />';
+		if(is_dir(THEME_DIR.'/'.$file.'/cs')){
+			$dir2=new DirectoryIterator(THEME_DIR.'/'.$file.'/cs');
+			echo 'variant: <select name="theme_variant">';
+			foreach($dir2 as $file2){
+				if($file2->isDot())continue;
+				$file2=preg_replace('/\.css$/','',$file2);
+				$sel=$file2==$DBVARS['theme_variant']?' selected="selected"':'';
+				echo '<option',$sel,'>',htmlspecialchars($file2).'</option>';
+			}
+			echo '</select>';
+		}
+		echo '<br /><input type="submit" value="'.__('set theme').'" /></form></div>';
 	}
 	if($themes_found==0){
 		echo '<em>No themes found. Download a theme and unzip it into the /ww.skins/ directory.</em>';
