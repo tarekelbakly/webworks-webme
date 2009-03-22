c5957cdc824f16a889fcec14ae0ad0f6cc2bc12a
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-03-22 20:29:22 +0000

    remove existing logo thumbnails when uploading a new logo try not to cache the logo so it shows in the admin area.

--- trunk/ww.admin/siteoptions/general.php
+++ trunk/ww.admin/siteoptions/general.php
@@ -6,9 +6,9 @@ if($action=='Save'){
 	$DBVARS['site_subtitle']=$_REQUEST['site_subtitle'];
 	if(isset($_FILES['site_logo']) && file_exists($_FILES['site_logo']['tmp_name'])){
 		$tmpname=addslashes($_FILES['site_logo']['tmp_name']);
-		$newname=USERBASE.'/f/skin_files/logo.png';
+		$newdir=USERBASE.'/f/skin_files';
 		mkdir(USERBASE.'/f/skin_files');
-		`convert "$tmpname" -geometry 320x320 "$newname"`;
+		`rm -fr "$newdir"/logo-* ; convert "$tmpname" -geometry 320x320 "$newdir/logo.png"`;
 	}
 	config_rewrite();
 	echo '<em>'.__('options updated').'</em>';
@@ -18,6 +18,6 @@ if($action=='Save'){
 echo '<form method="post" action="siteoptions.php?page=general" enctype="multipart/form-data"><input type="hidden" name="MAX_FILE_SIZE" value="9999999" /><table>';
 echo '<tr><th>Website Title</th><td><input name="site_title" value="'.htmlspecialchars($DBVARS['site_title']).'" /></td></tr>';
 echo '<tr><th>Website Subtitle</th><td><input name="site_subtitle" value="'.htmlspecialchars($DBVARS['site_subtitle']).'" /></td></tr>';
-echo '<tr><th>Logo</th><td><input type="file" name="site_logo" />',file_exists(USERBASE.'f/skin_files/logo.png')?'<img src="/f/skin_files/logo.png" />':'','</td></tr>';
+echo '<tr><th>Logo</th><td><input type="file" name="site_logo" /><br />',file_exists(USERBASE.'f/skin_files/logo.png')?'<img src="/f/skin_files/logo.png?rand='.mt_rand(0,9999).'" />':'','</td></tr>';
 echo '</table><input type="submit" name="action" value="Save" /></form>';
 // }
