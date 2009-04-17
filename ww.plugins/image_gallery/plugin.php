bfa33cc9b32c6016dfdec555bd7cccc205e4a859
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-04-17 09:37:23 +0000

    more options for the gallery

--- ww.plugins/image_gallery/plugin.php
+++ ww.plugins/image_gallery/plugin.php
@@ -30,9 +30,12 @@ function image_gallery_get_subdirs($base,$dir){
 }
 function image_gallery_admin_page_form($page,$vars){
 	$gvars=array(
-		'image_gallery_directory'=>'/',
-		'image_gallery_x'=>3,
-		'image_gallery_y'=>2
+		'image_gallery_directory'    =>'/',
+		'image_gallery_x'            =>3,
+		'image_gallery_y'            =>2,
+		'image_gallery_thumbsize'    =>150,
+		'image_gallery_captionlength'=>100,
+		'image_gallery_hoverphoto'   =>0
 	);
 	foreach($gvars as $n=>$v)if(isset($vars[$n]))$gvars[$n]=$vars[$n];
 	$cssurl=false;
@@ -50,10 +53,17 @@ function image_gallery_admin_page_form($page,$vars){
 		if($d==@$gvars['image_gallery_directory'])$c.=' selected="selected"';
 		$c.='>'.htmlspecialchars($d).'</option>';
 	}
-	$c.='</select><a href="#page_vars[image_gallery_directory]" onclick="javascript:window.open(\'/j/'.FCKEDITOR.'/editor/plugins/kfm/?startup_folder=\'+$(\'#image_gallery_directory\').attr(\'value\'),\'kfm\',\'modal,width=800,height=600\');">Manage Images</a></td>';
-	$c.='<th>'.__('Columns').'</th><td><input name="page_vars[image_gallery_x]" value="'.(int)$gvars['image_gallery_x'].'" /></td>';
-	$c.='<th>'.__('Rows').'</th><td><input name="page_vars[image_gallery_y]" value="'.(int)$gvars['image_gallery_y'].'" /></td>';
-	$c.='</tr></table>';
+	$c.='</select></td>';
+	$c.='<td colspan="2"><a style="background:#ff0;font-weight:bold;color:red;display:block;text-align:center;" href="#page_vars[image_gallery_directory]" onclick="javascript:window.open(\'/j/'.FCKEDITOR.'/editor/plugins/kfm/?startup_folder=\'+$(\'#image_gallery_directory\').attr(\'value\'),\'kfm\',\'modal,width=800,height=600\');">Manage Images</a></td></tr>';
+	$c.='<tr><th>'.__('Columns').'</th><td><input name="page_vars[image_gallery_x]" value="'.(int)$gvars['image_gallery_x'].'" /></td>';
+	$c.='<th>'.__('Rows').'</th><td><input name="page_vars[image_gallery_y]" value="'.(int)$gvars['image_gallery_y'].'" /></td></tr>';
+	$cl=(int)@$gvars['image_gallery_captionlength'];
+	$cl=$cl?$cl:100;
+	$c.='<tr><th>'.__('Caption Length').'</th><td><input name="page_vars[image_gallery_captionlength]" value="'.$cl.'" /></td>';
+	$ts=(int)@$gvars['image_gallery_thumbsize'];
+	$ts=$ts?$ts:150;
+	$c.='<th>'.__('Thumb Size').'</th><td><input name="page_vars[image_gallery_thumbsize]" value="'.$ts.'" /></td></tr>';
+	$c.='</table>';
 	$c.='</div>';
 	// }
 	$c.='</div>';
