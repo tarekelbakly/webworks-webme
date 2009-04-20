bde41f8d6f0ba189ade71e4b219f016ee3004260
conormacaoidh <conormacaoidh@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-04-20 19:34:03 +0000

    # forgot to add upgrade.php oops

---
+++ ww.plugins/banner-image/upgrade.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Webme Banner Image Plugin v0.1
+	File: upgrade.php
+	Developer: Conor Mac Aoidh <http://macaoidh.name>
+	Report Bugs: <conor@macaoidh.name>
+*/
+
+require SCRIPTBASE.'ww.incs/db.php';
+if($version==0) $version='0.1';
+$DBVARS[$pname.'|version']=$version;
+config_rewrite();
+?>
