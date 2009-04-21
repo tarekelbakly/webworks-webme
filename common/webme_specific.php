6f01dfe52e6ea929858a9ac390d226eb53945573
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-04-21 12:08:04 +0000

    missing mail.php... I should follow the same advise I gave Conor!

--- common/webme_specific.php
+++ common/webme_specific.php
@@ -5,27 +5,10 @@ function admin_can_create_top_pages(){
 function config_rewrite(){
 	global $DBVARS;
 	$tmparr=$DBVARS;
-	$tmparr2=array();
 	$tmparr['plugins']=join(',',$DBVARS['plugins']);
+	$tmparr2=array();
 	foreach($tmparr as $name=>$val)$tmparr2[]='\''.addslashes($name).'\'=>\''.addslashes($val).'\'';
 	$config="<?php\n\$DBVARS=array(\n	".join(",\n	",$tmparr2)."\n);";
-/*	$config='<'."?php
-\$DBVARS=array(
-	'username'     => '".addslashes($DBVARS['username'])."',
-	'password'     => '".addslashes($DBVARS['password'])."',
-	'hostname'     => '".addslashes($DBVARS['hostname'])."',
-	'db_name'      => '".addslashes($DBVARS['db_name'])."',
-
-	'theme'        => '".addslashes($DBVARS['theme'])."',
-	'theme_dir'    => '".addslashes($DBVARS['theme_dir'])."',
-	'theme_variant'=> '".addslashes($DBVARS['theme_variant'])."',
-
-	'site_title'   => '".addslashes($DBVARS['site_title'])."',
-	'site_subtitle'=> '".addslashes($DBVARS['site_subtitle'])."',
-	'version'      => ".((int)$DBVARS['version']).",
-	'userbase'     => '".addslashes($DBVARS['userbase'])."',
-	'plugins'      => '".join(',',$DBVARS['plugins'])."'
-);";*/
 	file_put_contents(CONFIG_FILE,$config);
 }
 function is_admin(){
