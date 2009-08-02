4fa28d282a2e4b915588925981f38ed0f08e899b
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-08-02 18:32:03 +0000

    fix a few warnings from page creation and editing

--- ww.admin/pages/pages.action.new.php
+++ ww.admin/pages/pages.action.new.php
@@ -9,30 +9,31 @@ if(allowedToEditPage($parent)){
 		echo '<em>'.__('A page named "%1" already exists. Page name amended to "%2"',$name,$name.$i).'</em>';
 		$name.=$i;
 	}
-	{ # variables
-		$template=getVar('template');
-		$type=getVar('type');
-		$body=str_replace(
-			array(
-				'<u />',"</ul>\r\n<ul>",'<u></u>',' align="center"','margin: 0cm 0cm 0pt;',
-				'class="Bodytext"','<span>&nbsp; </span>',' lang="EN-US"',' lang="EN-IE"',' class="MsoNormal"',
-				'style=""','bgstyle="color:','bgcolor="',' lang="EN-GB"'),
-			array(
-				'','','','','margin:0;',
-				'','','&nbsp;','','','',
-				'','style="background:','style="background:',''),
-			getVar('body')
-		);
-		$body=preg_replace('#</?([ovw]|st1):[^>]*>#','',$body);
-		$keywords=getVar('keywords');
-		$description=getVar('description');
-		$importance=(float)getVar('importance');
-		if($importance<0)$importance=0;
-		if($importance>1)$importance=1;
-		$category1=getVar('category1');
-		$category2=getVar('category2');
-		$category=$category2&&$category2!=__('add another')?$category2:$category1;
-	}
+	// { variables
+	$template=getVar('template');
+	$type=getVar('type');
+	$body=str_replace(
+		array(
+			'<u />',"</ul>\r\n<ul>",'<u></u>',' align="center"','margin: 0cm 0cm 0pt;',
+			'class="Bodytext"','<span>&nbsp; </span>',' lang="EN-US"',' lang="EN-IE"',' class="MsoNormal"',
+			'style=""','bgstyle="color:','bgcolor="',' lang="EN-GB"'),
+		array(
+			'','','','','margin:0;',
+			'','','&nbsp;','','','',
+			'','style="background:','style="background:',''),
+		getVar('body')
+	);
+	$title=isset($_REQUEST['title'])?addslashes($_REQUEST['title']):'';
+	$body=preg_replace('#</?([ovw]|st1):[^>]*>#','',$body);
+	$keywords=getVar('keywords');
+	$description=getVar('description');
+	$importance=(float)getVar('importance');
+	if($importance<0)$importance=0;
+	if($importance>1)$importance=1;
+	$category1=getVar('category1');
+	$category2=getVar('category2');
+	$category=$category2&&$category2!=__('add another')?$category2:$category1;
+	// }
 	if(getVar('page_order')==0){
 		dbQuery("update pages set ord=ord+1 where parent=".$pid);
 		$ord=0;
@@ -40,14 +41,14 @@ if(allowedToEditPage($parent)){
 	else{
 		$ord=dbOne('select ord from pages where parent='.$pid.' order by ord desc limit 1','ord')+1;
 	}
-	$q='insert into pages set ord="'.$ord.'",importance="'.$importance.'",category="'.$category.'",keywords="'.$keywords.'",description="'.$description.'",cdate=now(),template="'.$template.'",edate=now(),name="'.$name.'",title="'.$_POST['title'].'",body="'.addslashes(sanitise_html(getVar('body'))).'",type="'.$type.'"';
+	$q='insert into pages set ord="'.$ord.'",importance="'.$importance.'",category="'.$category.'",keywords="'.$keywords.'",description="'.$description.'",cdate=now(),template="'.$template.'",edate=now(),name="'.$name.'",title="'.$title.'",body="'.addslashes(sanitise_html(getVar('body'))).'",type="'.$type.'"';
 	$q.=',parent='.$pid;
 	if(has_page_permissions(128))$q.=',special='.$special;else $q.=',special=0';
 	dbQuery($q);
 	$id=dbOne('select last_insert_id() as id','id');
 	dbQuery('insert into permissions set id="'.$id.'", type=1, value="'.get_userid().'=7'."\n\n4".'"');
 	echo '<em>'.__('An item has been added to the database.').'</em>';
-	mysql_query('update page_summaries set rss=""');
+	dbQuery('update page_summaries set rss=""');
 	cache_clear('menus');
 	cache_clear('pages');
 }
