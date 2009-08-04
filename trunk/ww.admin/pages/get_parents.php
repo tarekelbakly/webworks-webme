f00b251840a2ed89d8585671345781d60253f1b5
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-08-04 09:16:46 +0000

    damn. missed putting this file in

---
+++ trunk/ww.admin/pages/get_parents.php
@@ -0,0 +1,8 @@
+<?php
+require '../../ww.incs/basics.php';
+require '../../ww.incs/db.php';
+require 'pages.funcs.php';
+$selected=isset($_REQUEST['selected'])?$_REQUEST['selected']:0;
+$id=isset($_REQUEST['other_GET_params'])?(int)$_REQUEST['other_GET_params']:-1;
+echo '<option value="0">--  '.__('none').'  --</option>';
+selectkiddies(0,0,$selected,$id);
