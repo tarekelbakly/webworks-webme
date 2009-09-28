5115926770203222aaf30a798e7a22c7dbc96b46
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-09-28 13:21:37 +0000

    workaround for jquery-ui bug: http://dev.jqueryui.com/ticket/4429

--- ww.plugins/forms/j/admin.fields.js
+++ ww.plugins/forms/j/admin.fields.js
@@ -3,6 +3,9 @@ function formfieldsAddRow(){
 	formfieldElements++;
 	$('<li><table width="100%"><tr><td width="30%"><input name="formfieldElementsName['+formfieldElements+']" /></td><td width="30%"><select name="formfieldElementsType['+formfieldElements+']"><option>'+form_input_types.join('</option><option>')+'</option></select></td><td width="10%"><input type="checkbox" name="formfieldElementsIsRequired['+formfieldElements+']" /></td><td></td></tr></table></li>').appendTo($('#form_fields'));
 	$('#form_fields').sortable();
+	$('#form_fields input').bind('click.sortable mousedown.sortable',function(ev){
+		ev.target.focus();
+	});
 }
 function formfieldsChange(e){
 }
