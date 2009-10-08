9c7085247207281e0b689f6ef6a93821da445921
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-10-08 09:54:36 +0000

    couldn't replicate it, but was told that select boxes were not clickable in sortable forms. I've added a click event to the selectboxes anyway just to be certain

--- ww.plugins/forms/j/admin.fields.js
+++ ww.plugins/forms/j/admin.fields.js
@@ -3,7 +3,7 @@ function formfieldsAddRow(){
 	formfieldElements++;
 	$('<li><table width="100%"><tr><td width="30%"><input name="formfieldElementsName['+formfieldElements+']" /></td><td width="30%"><select name="formfieldElementsType['+formfieldElements+']"><option>'+form_input_types.join('</option><option>')+'</option></select></td><td width="10%"><input type="checkbox" name="formfieldElementsIsRequired['+formfieldElements+']" /></td><td></td></tr></table></li>').appendTo($('#form_fields'));
 	$('#form_fields').sortable();
-	$('#form_fields input').bind('click.sortable mousedown.sortable',function(ev){
+	$('#form_fields input,#form_fields select').bind('click.sortable mousedown.sortable',function(ev){
 		ev.target.focus();
 	});
 }
