77d8d2f4e09f7571996acbdc40eb108f83150333
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-10-27 12:01:55 +0000

    fix broken "default" handler

--- ww.plugins/banner-image/frontend/banner-image.php
+++ ww.plugins/banner-image/frontend/banner-image.php
@@ -9,14 +9,14 @@
 function show_banner($vars){
 	if(!is_array($vars) && isset($vars->id) && $vars->id){
 		$b=dbRow('select * from banners_images where id='.$vars->id);
-		if(count($b) && !$b['html']){
+		if($b && count($b) && !$b['html']){
 			$b['html']=banner_image_getImgHTML($vars->id);
 			dbQuery('update banners_pages set html="'.addslashes($b['html']).'" where id='.$vars->id);
 		}
 	}
 	else if($GLOBALS['PAGEDATA']->id){
 		$b=dbRow('select * from banners_pages,banners_images where pageid='.$GLOBALS['PAGEDATA']->id.' and bannerid=id order by rand() limit 1');
-		if(count($b) && !$b['html']){
+		if($b && count($b) && !$b['html']){
 			$b['html']=banner_image_getImgHTML($b['id']);
 			dbQuery('update banners_pages set html="'.addslashes($b['html']).'" where id='.$b['id']);
 		}
@@ -24,7 +24,7 @@ function show_banner($vars){
 	if(!isset($b) || !count($b)){
 		$b=dbRow('select * from banners_images where !pages order by rand() limit 1');
 	}
-	if(count($b)){
+	if($b && count($b)){
 		$banner=$b['html'];
 		if(!$banner)$banner=banner_image_getImgHTML($vars->id);
 	}
