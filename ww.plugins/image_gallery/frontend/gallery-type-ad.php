2afc178b73da41c85e06f5869e87b84ed6f84f0c
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-11-10 08:23:39 +0000

    if someone puts "\n" in an image caption, it will now convert to a carriage return in the gallery

--- ww.plugins/image_gallery/frontend/gallery-type-ad.php
+++ ww.plugins/image_gallery/frontend/gallery-type-ad.php
@@ -1,7 +1,7 @@
 <?php
 $c.='<div class="ad-gallery"> <div class="ad-image-wrapper"> </div> <div class="ad-controls"> </div> <div class="ad-nav"> <div class="ad-thumbs"> <ul class="ad-thumb-list">';
 for($i=0;$i<$n;$i++){
-	$c.='<li> <a href="/kfmget/'.$images[$i]['id'].'"> <img src="/kfmget/'.$images[$i]['id'].',width='.$vars['image_gallery_thumbsize'].',height='.$vars['image_gallery_thumbsize'].'" title="'.$images[$i]['caption'].'"> </a> </li>';
+	$c.='<li> <a href="/kfmget/'.$images[$i]['id'].'"> <img src="/kfmget/'.$images[$i]['id'].',width='.$vars['image_gallery_thumbsize'].',height='.$vars['image_gallery_thumbsize'].'" title="'.str_replace('\\\\n','<br />',$images[$i]['caption']).'"> </a> </li>';
 }
 $c.='</ul> </div> </div> </div>';
 $c.='<script src="/ww.plugins/image_gallery/j/ad-gallery/jquery.ad-gallery.pack.js"></script><style type="text/css">@import "/ww.plugins/image_gallery/j/ad-gallery/jquery.ad-gallery.css";.ad-gallery .ad-image-wrapper{	height: 400px;}</style><script>
