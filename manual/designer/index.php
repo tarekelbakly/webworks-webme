231d873795a47838ff4824204a5f0cac4b5e3e83
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2009-11-12 11:40:45 +0000

    add beginnings of manual

---
+++ manual/designer/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="/css/styles.css" />
+	</head>
+	<body>
+		<h1>WebWorks WebME manual</h1>
+		<h2>Designer Documentation</h2>
+		<ul>
+			<li><a href="template-codes.php">Template Codes</a></li>
+		</ul>
+	</body>
+</html>
