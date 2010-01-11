36c0f9943625a5b87da36a15f3824d02289d8318
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-01-11 12:34:49 +0000

    slight advance in online store plugin. ignore - just moving from office machine to home machine

--- ww.plugins/online-store/admin/index.php
+++ ww.plugins/online-store/admin/index.php
@@ -1,5 +1,44 @@
 <?php
 $c='<div class="tabs">';
+// { orders
+$c.='<div class="tabPage"><h2>Orders</h2>';
+if(!isset($_SESSION['online-store']))$_SESSION['online-store']=array();
+if(!isset($_SESSION['online-store']['status']))$_SESSION['online-store']['status']=1;
+if(isset($_REQUEST['online-store-status']))$_SESSION['online-store']['status']=(int)$_REQUEST['online-store-status'];
+$c.='<p>This list shows orders with the status: <select id="online-store-status">';
+$arr=array('Unpaid','Paid','Paid and Delivered');
+foreach($arr as $k=>$v){
+	$c.='<option value="'.$k.'"';
+	if($k==$_SESSION['online-store']['status'])$c.=' selected="selected"';
+	$c.='">'.htmlspecialchars($v).'</option>';
+}
+$c.='</select>.</p>';
+$rs=dbAll('select id,total,date_created from online_store_orders where status='.((int)$_SESSION['online-store']['status']).' order by date_created desc');
+if(is_array($rs) && count($rs)){
+	$c.='<div style="margin:0 20%"><table width="100%" class="datatable"><thead><tr><th>Date</th><th>Amount</th><th>Invoice</th><th>Status</th></tr></thead><tbody>';
+	foreach($rs as $r){
+		$c.='<tr><td><span style="display:none">'.$r['date_created'].'</span>'.date_m2h($r['date_created']).'</td><td>&euro;'.$r['total'].'</td><td><a href="javascript:os_invoice('.$r['id'].')">invoice</a></td><td><a href="javascript:os_status('.$r['id'].')">'.htmlspecialchars($arr[(int)$r['status']]).'</a></td></tr>';
+	}
+	$c.='</tbody></table></div>';
+}
+else $c.='<em>No orders with this status exist.</em>';
+$c.='</div>';
+/*
+mysql> describe online_store_orders;
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| id           | int(11)     | NO   | PRI | NULL    | auto_increment | 
+| form_vals    | text        | YES  |     | NULL    |                | 
+| invoice      | text        | YES  |     | NULL    |                | 
+| total        | float       | YES  |     | NULL    |                | 
+| date_created | datetime    | YES  |     | NULL    |                | 
+| items        | text        | YES  |     | NULL    |                | 
+| status       | smallint(6) | YES  |     | 0       |                | 
++--------------+-------------+------+-----+---------+----------------+
+7 rows in set (0.00 sec)
+*/
+// }
 // { form
 $c.='<div class="tabPage"><h2>Form</h2>';
 $c.='<p>This is the form that will be presented as the checkout.</p>';
@@ -32,6 +71,7 @@ $c.='</div>';
 $c.='</div>';
 // }
 $c.='</div>';
+$c.='<script src="/ww.plugins/online-store/j/admin.js"></script>';
 
 if(!file_exists(USERBASE.'ww.cache/online-store'))mkdir(USERBASE.'ww.cache/online-store');
 if(file_exists(USERBASE.'ww.cache/online-store/'.$page['id']))unlink(USERBASE.'ww.cache/online-store/'.$page['id']);
