4cf9bb5e8186681650e43a51f5862de431b0f7cd
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-02-17 09:34:20 +0000

    modal dialogs for alerts, instead of hiding them in the page text

--- ww.plugins/privacy/admin/page_type.php
+++ ww.plugins/privacy/admin/page_type.php
@@ -53,6 +53,28 @@ $html.='</select></td></tr>';
 // }
 $html.='</table></div>';
 // }
+// { messages
+$html.='<div class="tabPage"><h2>Messages</h2><div class="tabs">';
+// { Login header
+$html.='<div class="tabPage"><h2>Login</h2><p>This message appears above the login form.</p>';
+if(!isset($page_vars['userlogin_message_login']))$page_vars['userlogin_message_login']='<p>Please log in using your email address and password. If you don\'t already have a user account, please use the Register tab (see above) to register.</p>';
+$html.=ckeditor('page_vars[userlogin_message_login]',$page_vars['userlogin_message_login'],false);
+$html.='</div>';
+// }
+// { Reminder header
+$html.='<div class="tabPage"><h2>Reminder</h2><p>This message appears above the password reminder form.</p>';
+if(!isset($page_vars['userlogin_message_reminder']))$page_vars['userlogin_message_reminder']='<p>If you have forgotten your password, please enter your email address here to have a new verification email sent out to you.</p>';
+$html.=ckeditor('page_vars[userlogin_message_reminder]',$page_vars['userlogin_message_reminder'],false);
+$html.='</div>';
+// }
+// { Register header
+$html.='<div class="tabPage"><h2>Registration</h2><p>This message appears above the user registration form.</p>';
+if(!isset($page_vars['userlogin_message_registration']))$page_vars['userlogin_message_registration']='<p>Please enter your name and email address. After submitting, please check your email account for your account verification link.</p>';
+$html.=ckeditor('page_vars[userlogin_message_registration]',$page_vars['userlogin_message_registration'],false);
+$html.='</div>';
+// }
+$html.='</div></div>';
+// }
 // { terms and conditions
 $html.='<div class="tabPage"><h2>Terms and Conditions</h2><p>Leave blank if no terms and conditions agreement is needed</p>';
 $html.=ckeditor('page_vars[userlogin_terms_and_conditions]',$page_vars['userlogin_terms_and_conditions'],false);
--- ww.plugins/privacy/frontend/page_type.php
+++ ww.plugins/privacy/frontend/page_type.php
@@ -11,7 +11,8 @@ function userloginandregistrationDisplay(){
 		if(!count($r))die('that hash and email combination does not exist');
 		$password=Password::getNew();
 		dbQuery("update user_accounts set password=md5('$password'),verification_hash='',active=1 where email='".addslashes($_GET['email'])."' and verification_hash='".addslashes($_GET['hash'])."'");
-		$c.='<h1>Thank you</h1><p>Your email address has been verified. Your login password is <strong>'.$password.'</strong>. Please take note of this password, and then log in.</p>';
+		$m='<h1>Thank you</h1><p>Your email address has been verified. Your login password is <strong>'.$password.'</strong>. Please take note of this password, and then log in.</p>';
+		$c.=$m.'<script>$("<div>'.$m.'</div>").dialog({modal:true});</script>';
 	}
 	if($action=='Login' || $loggedin){
 		// { variables
@@ -89,7 +90,7 @@ function userLoginBoxDisplay(){
 	global $PAGEDATA;
 	$c='<div id="userLoginBoxDisplay"><h2>Login</h2>';
 	if(getVar('action')=='Login')$c.='<em>incorrect email or password given.</em>';
-	if($PAGEDATA->vars['userlogin_visibility']&2)$c.='<em>Don\'t have a user account? Please use the Register form (see tabs above)</em>';
+	if(isset($PAGEDATA->vars['userlogin_message_login']))$c.=$PAGEDATA->vars['userlogin_message_login'];
 	$c.='<form class="userLoginBox" action="'.$GLOBALS['PAGEDATA']->getRelativeUrl().'#tab=Login" method="post"><table>';
 	$c.='<tr><th><label for="email">Email</label></th><td><input type="text" name="email" value="'.getVar('email').'" /></td>';
 	$c.='<th><label for="password">Password</label></th><td><input type="password" name="password" /></td></tr>';
@@ -99,7 +100,9 @@ function userLoginBoxDisplay(){
 	return $c;
 }
 function userPasswordReminder(){
+	global $PAGEDATA;
 	$c='<div id="userPasswordReminder"><h2>Password Reminder</h2>';
+	if(isset($PAGEDATA->vars['userlogin_message_reminder']))$c.=$PAGEDATA->vars['userlogin_message_reminder'];
 	$c.='<form class="userLoginBox" action="'.$GLOBALS['PAGEDATA']->getRelativeUrl().'#tab=Password Reminder" method="post"><table>';
 	$c.='<tr><th><label for="email">Email</label></th><td><input type="text" name="email" /></td></tr></table>';
 	$c.='<input type="submit" name="action" value="Remind" /></form>';
@@ -112,7 +115,9 @@ function userregistration(){
 }
 function userregistration_form($error=''){
 	global $PAGEDATA;
-	$c='<div id="userregistration"><h2>Register</h2>'.$error.'<form class="userRegistrationBox" action="'.$GLOBALS['PAGEDATA']->getRelativeUrl().'#tab=Register" method="post"><table>'
+	$c='<div id="userregistration"><h2>Register</h2>';
+	if(isset($PAGEDATA->vars['userlogin_message_registration']))$c.=$PAGEDATA->vars['userlogin_message_registration'];
+	$c.=$error.'<form class="userRegistrationBox" action="'.$GLOBALS['PAGEDATA']->getRelativeUrl().'#tab=Register" method="post"><table>'
 		.'<tr><th>Name</th><td><input type="text" name="name" value="'.htmlspecialchars(getVar('name')).'" /></td>'
 		.'<th>Email</th><td><input type="text" name="email" value="'.htmlspecialchars(getVar('email')).'" /></td></tr></table>';
 	if(isset($PAGEDATA->vars['userlogin_terms_and_conditions']) && $PAGEDATA->vars['userlogin_terms_and_conditions']){
@@ -165,21 +170,21 @@ function userregistration_register(){
 		$sesc=urlencode($short_url);
 		dbQuery("insert into short_urls values(0,now(),'$lesc','$short_url')");
 		if(@$page->vars['userlogin_registration_type']=='Email-verified'){
-    	mail($email,'['.$sitedomain.'] user registration',"Hello!\n\nThis message is to verify your email address, which has been used to register a user-account on the $sitedomain website.\n\nIf you did not, then please delete this email. Otherwise, please click the following URL to verify your email address with us. Thank you.\n\nhttp://$sitedomain/_s/".$sesc,"From: noreply@$sitedomain_s\nReply-to: noreply@$sitedomain_s");
+    	mail($email,'['.$sitedomain.'] user registration',"Hello!\n\nThis message is to verify your email address, which has been used to register a user-account on the $sitedomain website.\n\nIf you did not register this account, then please delete this email. Otherwise, please click the following URL to verify your email address with us. Thank you.\n\nhttp://$sitedomain/_s/".$sesc,"From: noreply@$sitedomain_s\nReply-to: noreply@$sitedomain_s");
 			if(1 || $page->vars['userlogin_send_admin_emails']){
 				$admins=dbAll('select email from user_accounts,users_groups where groups_id=1 && user_accounts_id=user_accounts.id');
 				foreach($admins as $admin){
 					mail($admin['email'],'['.$sitedomain.'] user registration',"Hello!\n\nThis message is to alert you that a user has been created on your site, http://$sitedomain/ - the user has not yet been activated, so please log into the admin area of the site (http://$sitedomain/ww.admin/ - under Site Options then Users) and verify that the user details are correct.","From: noreply@$sitedomain_s\nReply-to: noreply@$sitedomain_s");
 				}
 			}
-			return '<p><strong>Thank you for registering</strong>. Please check your email for a verification URL. Once that\'s been followed, your account will be activated and a password supplied to you.</p>';
+			return '<script>$("<p><strong>Thank you for registering</strong>. Please check your email for a verification URL. Once that\'s been followed, your account will be activated and a password supplied to you.</p>").dialog({modal:true});</script>';
 		}
 		else{
 			$admins=dbAll('select email from user_accounts,users_groups where groups_id=1 && user_accounts_id=user_accounts.id');
 			foreach($admins as $admin){
 				mail($admin['email'],'['.$sitedomain.'] user registration',"Hello!\n\nThis message is to alert you that a user ($email) has been created on your site, http://$sitedomain/ - the user has not yet been activated, so please log into the admin area of the site (http://$sitedomain/ww.admin/ - under Site Options then Users) and verify that the user details are correct.","From: noreply@$sitedomain_s\nReply-to: noreply@$sitedomain_s");
 			}
-			return '<p><strong>Thank you for registering</strong>. Our admins will moderate your registration, and you will receive an email with your new password when it is activated.</p>';
+			return '<script>$("<p><strong>Thank you for registering</strong>. Our admins will moderate your registration, and you will receive an email with your new password when it is activated.</p>").dialog({modal:true});</script>';
 		}
 	// }
 }
