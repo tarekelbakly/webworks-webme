e121610f4de2f389cec099aa40800cf87d3dfc7e
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-10 08:23:12 +0000

    add new "Protected Files" plugin
    this plugin allows an admin to protect a directory of downloads, such that all requests will ask for an email address (not verified) before allowing the download to proceed. All downloads are sent as emails to a recipient.
    
    creates a admin section "Misc>Protected Files"

--- ww.incs/common.php
+++ ww.incs/common.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__FILE__).'/basics.php';
+require_once dirname(__FILE__).'/basics.php';
 require_once SCRIPTBASE . 'ww.incs/Smarty-2.6.26/libs/Smarty.class.php';
 function date_m2h($d, $type = 'date') {
 	$date = preg_replace('/[- :]/', ' ', $d);
