e55bfcbf151d028f19e6a51ac6c04f18136e2b77
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-14 11:52:53 +0000

    fix onetouchcontact widget - a few missing <br>s in the form

--- ww.plugins/onetouchcontact/frontend/widget.php
+++ ww.plugins/onetouchcontact/frontend/widget.php
@@ -1,7 +1,7 @@
 <?php
 function onetouchcontact_widget_show($vars){
-	$form='<form class="onetouchcontact">Name<br /><input id="onetouchcontact-name" /><br />Email<br /><input id="onetouchcontact-email" />';
-	if($vars->phone)$form.='Phone<br /><input id="onetouchcontact-phone" />';
+	$form='<form class="onetouchcontact">Name<br /><input id="onetouchcontact-name" /><br />Email<br /><input id="onetouchcontact-email" /><br />';
+	if($vars->phone)$form.='Phone<br /><input id="onetouchcontact-phone" /><br />';
 	$form.='<input type="hidden" name="cid" value="'.$vars->cid.'" /><input type="hidden" name="mid" value="'.$vars->mid.'" />';
 	$form.='<div class="onetouchcontact-msg"></div><input type="submit" value="subscribe" /></form>';
 	$form.='<script src="/ww.plugins/onetouchcontact/frontend/js.js"></script>';
