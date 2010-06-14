c7e14e5d993b95deaf334c2b24ec016c8129fe5e
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-14 11:16:54 +0000

    fix installer (wrong field count for page installation)
    fix html sanitiser so it replaces <p><img blah></p> with <img blah>

--- trunk/upgrades/upgrade.php
+++ trunk/upgrades/upgrade.php
@@ -7,6 +7,7 @@ echo '<strong>upgrades detected. running upgrade script.</strong>';
 	if(isset($DBVARS['userbase']))$userbase=$DBVARS['userbase'];
 	else $userbase=SCRIPTBASE;
 // }
+
 if($version==0){ // missing user accounts and groups
 	// { user_accounts
 	dbQuery('create table user_accounts(
@@ -49,7 +50,7 @@ if($version==3){ // pages
 		`cdate` datetime NOT NULL default "0000-00-00 00:00:00", `special` bigint(20) default NULL, `edate` datetime default NULL, `title` text,
 		`htmlheader` text, `template` text, `type` smallint(6) default 0, `keywords` text, `description` text, `category` text NOT NULL, `importance` float default 0.5,
 		PRIMARY KEY  (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8');
-	dbQuery("INSERT INTO `pages` VALUES (1,'Home','<h1>Welcome</h1>\\r\\n<p>This is your new website. To administer it, please go to <a href=\\\"/ww.admin/\\\">/ww.admin</a> and log in using your email address and password. If you have forgotten your password, please use the reminder form to have a new password sent to you.</p>\\r\\n<p>If you don\\'t like the default theme, please choose a different one in the Site Options page.</p>\\r\\n<p>To keep uptodate with new developments in the CMS, please bookmark <a href=\\\"http://webme.eu/whats-new\\\">http://webme.eu/whats-new</a>.</p>',0,1,now(),1,now(),NULL,'',NULL,'',0,'','','',0.5)");
+	dbQuery("INSERT INTO `pages` VALUES (1,'Home','<h1>Welcome</h1>\\r\\n<p>This is your new website. To administer it, please go to <a href=\\\"/ww.admin/\\\">/ww.admin</a> and log in using your email address and password. If you have forgotten your password, please use the reminder form to have a new password sent to you.</p>\\r\\n<p>If you don\\'t like the default theme, please choose a different one in the Site Options page.</p>',0,1,now(),1,now(),'Welcome','','',0,'','','',0.5)");
 	$version=4;
 }
 if($version==4){ // page_vars
