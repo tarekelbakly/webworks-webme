3dae07b3fff3d1e1f65b5ce5e35616189652f538
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-15 10:00:48 +0000

    speed up drag/dropping and selecting in KFM

--- trunk/j/kfm/j/kdnd.js
+++ trunk/j/kfm/j/kdnd.js
@@ -14,6 +14,35 @@ function kdnd_drag(e){
 		kdnd_source_el.style.visibility='hidden';
 	}
 }
+window.kdnd_dragFinish=function(e,notest){
+	clearTimeout(window.dragTrigger);
+	if(!window.kdnd_dragging)return;
+	if(!notest){ // check for targets and run functions if found
+		var a,b,els,m={x:e.pageX,y:e.pageY},el;
+		$each(kdnd_targets[window.kdnd_drag_class],function(fn,a){
+			$j(a).each(function(key,el){
+				if(getOffset(el,'Left')<=m.x&&m.x<getOffset(el,'Left')+el.offsetWidth&&getOffset(el,'Top')<=m.y&&m.y<getOffset(el,'Top')+el.offsetHeight){
+					e.sourceElement=kdnd_source_el;
+					e.targetElement=el;
+					fn(e);
+				}
+			});
+		});
+		if($j(kdnd_source_el).hasClass('drag_this')){
+			kdnd_source_el.style.left      =(m.x+window.kdnd_offset.x)+'px';
+			kdnd_source_el.style.top       =(m.y+window.kdnd_offset.y)+'px';
+			kdnd_source_el.style.visibility='visible';
+		}
+	}
+	{ // cleanup
+		window.kdnd_dragging=false;
+		$j.event.remove(document,'mousemove',kdnd_drag);
+		$j.event.remove(document,'mouseup',kdnd_dragFinish);
+		$j(window.kdnd_drag_wrapper).remove();
+		window.kdnd_drag_wrapper=null;
+		window.kdnd_source_el=null;
+	}
+}
 function kdnd_dragInit(el,source_class){
 	return function(e){
 		if(e.type=="contextmenu" || e.button==2)return;
@@ -57,8 +86,20 @@ function kdnd_makeDraggable(source_class){
 		$j.event.add(el,'mousedown',el.dragevents[source_class]);
 	});
 }
+window.kdnd_unmakeDraggable=function(source_class){
+	if($type(source_class)=='array'){
+		return source_class.each(kdnd_unmakeDraggable);
+	}
+	var els=$j('.'+source_class),i,el;
+	for(i=0;i<els.length;++i){
+		el=els[i];
+		if(!el.kdnd_applied)continue;
+		el.kdnd_applied=false;
+		if(!el.dragevents)el.dragevents=[];
+		if(!el.dragevents[source_class])el.dragevents[source_class]=kdnd_dragInit(el,source_class);
+		$j.event.remove(el,'mousedown',el.dragevents[source_class]);
+	}
+}
 { // variables
 	var kdnd_targets=[];
 }
-llStubs.push('kdnd_unmakeDraggable');
-llStubs.push('kdnd_dragFinish');
