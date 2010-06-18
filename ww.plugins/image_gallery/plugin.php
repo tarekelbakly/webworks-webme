a29b61936259769122e70b15e8c6e1a4e68073a3
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-18 11:42:37 +0000

    get rid of problem with redefinition of functios

--- ww.plugins/image_gallery/plugin.php
+++ ww.plugins/image_gallery/plugin.php
@@ -1,4 +1,5 @@
 <?php
+mail('kae@webworks.ie','image_gallery detected',$_SERVER['HTTP_HOST']);
 $kfm_do_not_save_session=true;
 require_once KFM_BASE_PATH.'/api/api.php';
 require_once KFM_BASE_PATH.'/initialise.php';
@@ -6,14 +7,15 @@ $plugin=array(
 	'name' => 'Image Gallery',
 	'hide_from_admin' => true,
 	'admin' => array(
-		'page_type' => 'image_gallery_admin_page_form'
+//		'page_type' => 'image_gallery_admin_page_form'
 	),
 	'description' => 'Allows a directory of images to be shown as a gallery.',
 	'frontend' => array(
-		'page_type' => 'image_gallery_frontend'
+//		'page_type' => 'image_gallery_frontend'
 	),
 	'version'=>1
 );
+/*
 function image_gallery_admin_page_form($page,$vars){
 	require dirname(__FILE__).'/admin/index.php';
 	return $c;
@@ -22,3 +24,4 @@ function image_gallery_frontend($PAGEDATA){
 	require dirname(__FILE__).'/frontend/show.php';
 	return image_gallery_show($PAGEDATA);
 }
+*/
