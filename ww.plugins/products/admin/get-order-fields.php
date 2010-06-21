04a3ccd85ac68661f01944f3db60ab84070a3665
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-06-21 15:50:06 +0000

    products: update order_by fields if category is changed

--- ww.plugins/products/admin/get-order-fields.php
+++ ww.plugins/products/admin/get-order-fields.php
@@ -5,7 +5,21 @@ if(!is_admin())die('access denied');
 $fields=array();
 $filter='';
 if($_REQUEST['other_GET_params']){
-	$filter=' where id='.(int)$_REQUEST['other_GET_params'];
+	if(is_numeric($_REQUEST['other_GET_params'])){ // product type
+		$filter=' where id='.(int)$_REQUEST['other_GET_params'];
+	}
+	else if(strpos($_REQUEST['other_GET_params'],'c')===0){
+		$cat=(int)str_replace('c','',$_REQUEST['other_GET_params']);
+		$rs=dbAll('select product_id from products_categories_products where category_id='.$cat);
+		$arr=array();
+		foreach($rs as $r)$arr[]=$r['product_id'];
+		if(!count($arr))exit;
+		$rs=dbAll('select distinct product_type_id from products where id in ('.join(',',$arr).')');
+		$arr=array();
+		foreach($rs as $r)$arr[]=$r['product_type_id'];
+		if(!count($arr))exit;
+		$filter=' where id in ('.join(',',$arr).')';
+	}
 }
 $rs=dbAll('select data_fields from products_types'.$filter);
 foreach($rs as $r){
--- ww.plugins/products/admin/page-form.js
+++ ww.plugins/products/admin/page-form.js
@@ -13,14 +13,20 @@ $(function(){
 	$('#products_order_by_select').remoteselectoptions({
 		url:'/ww.plugins/products/admin/get-order-fields.php',
 		other_GET_params:function(){
-			if($('#products_what_to_show').val()=='1'){
-				return $('#products_what_to_show_1 select').val();
+			var val=$('#products_what_to_show').val();
+			switch(val){
+				case '1': // { product type
+					return $('#products_what_to_show_1 select').val();
+				// }
+				case '2': // { category
+					return 'c'+$('#products_what_to_show_2 select').val();
+				// }
 			}
 			return '';
 		},
 		always_retrieve:true
 	});
-	$('#products_what_to_show_1 select').change(function(){
+	$('#products_what_to_show_1 select,#products_what_to_show_2 select').change(function(){
 		$('#products_order_by_select').trigger('mousedown');
 	});
 });
