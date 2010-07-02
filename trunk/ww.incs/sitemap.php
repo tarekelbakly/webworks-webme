01dd39601d02d76774f3a06aa86ad392f4ca50fc
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-07-02 09:51:07 +0000

    in google sitemaps, use the current site domain. add short-form human-readable date to date_m2h (example: "Fri 2nd Jul, 2010")

--- trunk/ww.incs/sitemap.php
+++ trunk/ww.incs/sitemap.php
@@ -6,7 +6,7 @@ echo '<urlset xmlns="http://www.google.com/schemas/sitemap/0.84">';
 $rs=dbAll("select id,cdate,importance,name from pages where importance>0 order by importance desc");
 foreach($rs as $r){
 	$page=Page::getInstance($r['id']);
-	echo '<url><loc>http://'.$sitedomain.$page->getRelativeUrl().'</loc>'
+	echo '<url><loc>http://'.$_SERVER['HTTP_HOST'].$page->getRelativeUrl().'</loc>'
 		.'<lastmod>'.preg_replace('/ .*/','',$r['cdate']).'</lastmod>'
 		.'<priority>'.$r['importance'].'</priority>'
 		.'</url>';
