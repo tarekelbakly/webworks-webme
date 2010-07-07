feb9e56ab2ea75bd25fbb895d901ef55c582240b
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-07-07 14:50:47 +0000

    duplication of work? remove superfluous code

--- ww.plugins/online-store/verify/paypal.php
+++ ww.plugins/online-store/verify/paypal.php
@@ -37,30 +37,6 @@ if (!$fp) {
 			// process payment
 			require dirname(__FILE__).'/process-order.php';
 			process_order($id,$order);
-			// { mark order as paid
-			dbQuery("UPDATE online_store_orders SET status='1' WHERE id=$id");
-			// }
-			// { call the callback if it's supplied
-			if($order['callback'])file($order['callback']);
-			// }
-			$form_vals=json_decode($order['form_vals']);
-			$page=Page::getInstanceByType('online-store');
-			$page->initValues();
-			$from='noreply@'.str_replace('www.','',$_SERVER['HTTP_HOST']);
-			$bcc='';
-			$page=Page::getInstanceByType('online-store');
-			if($page && isset($page->vars['online_stores_admin_email']) && $page->vars['online_stores_admin_email']){
-				$from=$page->vars['online_stores_admin_email'];
-				$bcc=$page->vars['online_stores_admin_email'];
-			}
-			if(isset($form_vals->Email)){
-				$headers = "From: $from\r\nReply-To: $from\r\nX-Mailer: PHP/" . phpversion();
-				$headers.='MIME-Version: 1.0' . "\r\n";
-				$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-				$headers .= 'To: '.$form_vals->Email. "\r\n";
-				if($bcc)$headers.='BCC: '.$bcc."\r\n";
-				mail($form_vals->Email,'['.str_replace('www.','',$_SERVER['HTTP_HOST']).'] invoice #'.$id, $order['invoice'], $headers);
-			}
 		}
 		else if (strcmp ($res, "INVALID") == 0) {
 			// echo the response
@@ -70,4 +46,3 @@ if (!$fp) {
 	}
 	fclose ($fp);
 }
-?>
--- ww.plugins/online-store/verify/process-order.php
+++ ww.plugins/online-store/verify/process-order.php
@@ -1,11 +1,27 @@
 <?php
 function process_order($id,$order){
+	// { mark order as paid
 	dbQuery("UPDATE online_store_orders SET status='1' WHERE id=$id");
-	$form_vals=json_decode($order['form_vals'],true);
+	// }
+	// { call the callback if it's supplied
+	if($order['callback'])file($order['callback']);
+	// }
+	$form_vals=json_decode($order['form_vals']);
+	$page=Page::getInstanceByType('online-store');
+	$page->initValues();
 	$from='noreply@'.str_replace('www.','',$_SERVER['HTTP_HOST']);
-	$headers = "From: $from\r\nReply-To: $from\r\nX-Mailer: PHP/".phpversion()."\r\n";
-	$headers.='MIME-Version: 1.0'."\r\n";
-	$headers.= 'Content-type: text/html; charset=utf-8'."\r\n";
-	$headers.= 'To: '.$form_vals['Email']. "\r\n";
-	mail($form_vals['Email'],'['.str_replace('www.','',$_SERVER['HTTP_HOST']).'] invoice #'.$id, $order['invoice'], $headers);
+	$bcc='';
+	$page=Page::getInstanceByType('online-store');
+	if($page && isset($page->vars['online_stores_admin_email']) && $page->vars['online_stores_admin_email']){
+		$from=$page->vars['online_stores_admin_email'];
+		$bcc=$page->vars['online_stores_admin_email'];
+	}
+	if(isset($form_vals->Email)){
+		$headers = "From: $from\r\nReply-To: $from\r\nX-Mailer: PHP/" . phpversion();
+		$headers.='MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+		$headers .= 'To: '.$form_vals->Email. "\r\n";
+		if($bcc)$headers.='BCC: '.$bcc."\r\n";
+		mail($form_vals->Email,'['.str_replace('www.','',$_SERVER['HTTP_HOST']).'] invoice #'.$id, $order['invoice'], $headers);
+	}
 }
