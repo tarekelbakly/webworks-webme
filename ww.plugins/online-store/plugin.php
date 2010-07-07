bc085aeb7302d8b15b05efc421674bcdc57fbc3d
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-07-07 15:46:27 +0000

    clean up Online Store using phpcs
    still more to come, but it's a start...

--- ww.plugins/online-store/plugin.php
+++ ww.plugins/online-store/plugin.php
@@ -1,4 +1,15 @@
 <?php
+/**
+	* definition file for Online-Store plugin
+	*
+	* PHP version 5
+	*
+	* @category None
+	* @package  None
+	* @author   Kae Verens <kae@webworks.ie>
+	* @license  GPL 2.0
+	* @link     None
+*/
 // { define $plugin
 $plugin=array(
 	'name' => 'Online Store',
@@ -22,10 +33,30 @@ $online_store_currencies=array(
 	'GBP'=>array('&pound;','Pound Sterling')
 );
 // }
-function online_store_add_to_cart($cost=0,$amt=0,$short_desc='',$long_desc='',$md5='',$url=''){
+/**
+	* adds a product to the cart
+	*
+	* @param float  $cost       cost of the product
+	* @param int    $amt        how many to add
+	* @param string $short_desc short description of the product
+	* @param string $long_desc  long description of the product
+	* @param string $md5        a unique key for storing this product in the session
+	* @param string $url        URL where the product can be viewed
+	*
+	* @return null
+	*/
+function online_store_add_to_cart(
+				$cost=0, $amt=0, $short_desc='',
+				$long_desc='', $md5='', $url=''
+) {
 	// { add item to session
-	if(!isset($_SESSION['online-store']))$_SESSION['online-store']=array('items'=>array(),'total'=>0);
-	$item=(isset($_SESSION['online-store']['items'][$md5]))?$_SESSION['online-store']['items'][$md5]:array('cost'=>0,'amt'=>0,'short_desc'=>$short_desc,'long_desc'=>$long_desc,'url'=>$url);
+	if(!isset($_SESSION['online-store'])) {
+		$_SESSION['online-store']=array('items'=>array(),'total'=>0);
+	}
+	$item=(isset($_SESSION['online-store']['items'][$md5]))
+		?$_SESSION['online-store']['items'][$md5]
+		:array('cost'=>0,'amt'=>0,'short_desc'=>$short_desc,
+			'long_desc'=>$long_desc,'url'=>$url);
 	$item['cost']=$cost;
 	$item['amt']+=$amt;
 	$item['short_desc']=$short_desc;
@@ -35,57 +66,85 @@ function online_store_add_to_cart($cost=0,$amt=0,$short_desc='',$long_desc='',$m
 	require dirname(__FILE__).'/libs.php';
 	online_store_calculate_total();
 }
-function online_store_admin_page_form($page,$vars){
+/**
+	* admin are Page form
+	*
+	* @param object $page Page array from database
+	* @param array  $vars Page's custom variables
+	*
+	* @return string
+	*/
+function online_store_admin_page_form($page, $vars) {
 	require dirname(__FILE__).'/admin/index.php';
 	return $c;
 }
-function online_store_frontend($PAGEDATA){
+function online_store_frontend($PAGEDATA) {
 	require dirname(__FILE__).'/frontend/index.php';
 	return $c;
 }
-function online_store_generate_paypal_button($PAGEDATA,$id,$total){
+function online_store_generate_paypal_button($PAGEDATA, $id, $total) {
 	global $DBVARS;
-	return '<form id="online-store-paypal" method="post" action="https://www.paypal.com/cgi-bin/webscr"><input type="hidden" value="_xclick" name="cmd"/>'
-		.'<input type="hidden" value="'.$PAGEDATA->vars['online_stores_paypal_address'].'" name="business"/>'
-		.'<input type="hidden" value="Purchase made from '.$_SERVER['HTTP_HOST'].'" name="item_name"/>'
+	return '<form id="online-store-paypal" method="post" action="https://www.paypal.com'
+		.'/cgi-bin/webscr"><input type="hidden" value="_xclick" name="cmd"/>'
+		.'<input type="hidden" value="'.$PAGEDATA->vars['online_stores_paypal_address']
+		.'" name="business"/>'
+		.'<input type="hidden" value="Purchase made from '.$_SERVER['HTTP_HOST']
+		.'" name="item_name"/>'
 		.'<input type="hidden" value="'.$id.'" name="item_number"/>'
 		.'<input type="hidden" value="'.$total.'" name="amount"/>'
-		.'<input type="hidden" value="'.$DBVARS['online_store_currency'].'" name="currency_code"/><input type="hidden" value="1" name="no_shipping"/><input type="hidden" value="1" name="no_note"/>'
-		.'<input type="hidden" value="http://'.$_SERVER['HTTP_HOST'].'/ww.plugins/online-store/verify/paypal.php" name="notify_url"/>'
-		.'<input type="hidden" value="IC_Sample" name="bn"/><input type="image" alt="Make payments with payPal - it\'s fast, free and secure!" name="submit" src="https://www.paypal.com/en_US/i/btn/x-click-but23.gif"/><img width="1" height="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt=""/></form>';
+		.'<input type="hidden" value="'.$DBVARS['online_store_currency']
+		.'" name="currency_code"/><input type="hidden" value="1" name="no_shipping"/>'
+		.'<input type="hidden" value="1" name="no_note"/>'
+		.'<input type="hidden" value="http://'.$_SERVER['HTTP_HOST']
+		.'/ww.plugins/online-store/verify/paypal.php" name="notify_url"/>'
+		.'<input type="hidden" value="IC_Sample" name="bn"/><input type="image" alt="Make'
+		.' payments with payPal - it\'s fast, free and secure!" name="submit" src="https:'
+		.'//www.paypal.com/en_US/i/btn/x-click-but23.gif"/><img width="1" height="1" src='
+		.'"https://www.paypal.com/en_US/i/scr/pixel.gif" alt=""/></form>';
 }
-function online_store_pagedata(){
+function online_store_pagedata() {
 	$currency=$GLOBALS['DBVARS']['online_store_currency'];
 	$currency_symbols=array('EUR'=>'€','GBP'=>'£');
 	return ',"currency":"'.$currency_symbols[$currency].'"';
 }
-function online_store_show_basket_widget($vars=null){
+function online_store_show_basket_widget($vars=null) {
 	global $DBVARS,$online_store_currencies;
 	$csym=$online_store_currencies[$DBVARS['online_store_currency']][0];
 	$html='<div class="online-store-basket-widget">';
-	if(!isset($_SESSION['online-store']))$_SESSION['online-store']=array('items'=>array(),'total'=>0);
-	if(count($_SESSION['online-store']['items'])){
+	if (!isset($_SESSION['online-store'])) {
+		$_SESSION['online-store']=array('items'=>array(),'total'=>0);
+	}
+	if (count($_SESSION['online-store']['items'])) {
 		$html.='<table>';
 		$html.='<tr><th>&nbsp;</th><th>Price</th><th>Amount</th><th>Total</th></tr>';
-		foreach($_SESSION['online-store']['items'] as $md5=>$item){
+		foreach ($_SESSION['online-store']['items'] as $md5=>$item) {
 			// { name
 			$html.='<tr class="os_item_name"><td colspan="4">';
-			if($item['url'])$html.='<a href="'.$item['url'].'">';
+			if ($item['url']) {
+				$html.='<a href="'.$item['url'].'">';
+			}
 			$html.=$item['short_desc'];
-			if($item['url'])$html.='</a>';
+			if ($item['url']) {
+				$html.='</a>';
+			}
 			$html.='</td></tr>';
 			// }
-			$html.='<tr class="os_item_numbers" id="'.$md5.'"><td>&nbsp;</td><td>'.$csym.$item['cost'].'</td>';
+			$html.='<tr class="os_item_numbers" id="'.$md5.'"><td>&nbsp;</td><td>'
+				.$csym.$item['cost'].'</td>';
 			// { amount
 			$html.='<td class="amt">'.$item['amt'].'</td>';
 			// }
 			$html.='<td class="item-total">'.$csym.($item['cost']*$item['amt']).'</td></tr>';
 		}
-		$html.='<tr class="os_total"><th colspan="3">Total</th><td class="total">'.$csym.$_SESSION['online-store']['total'].'</td></tr>';
+		$html.='<tr class="os_total"><th colspan="3">Total</th><td class="total">'
+			.$csym.$_SESSION['online-store']['total'].'</td></tr>';
 		$html.='</table>';
-		$html.='<a href="/common/redirector.php?type=online-store">Proceed to Checkout</a>';
+		$html.='<a href="/common/redirector.php?type=online-store">'
+			.'Proceed to Checkout</a>';
 	}
-	else $html.='<em>empty</em>';
+	else {
+		$html.='<em>empty</em>';
+	}
 	$html.='</div><script src="/ww.plugins/online-store/j/basket.js"></script>';
 	return $html;
 }
--- ww.plugins/online-store/upgrade.php
+++ ww.plugins/online-store/upgrade.php
@@ -1,29 +1,44 @@
 <?php
-if($version==0){ // online_store_orders
-	dbQuery('CREATE TABLE IF NOT EXISTS `online_store_orders` (
-		`id` int(11) NOT NULL auto_increment,
-		`form_vals` text,
-		`invoice` text,
-		`total` float,
-		`date_created` datetime,
-		PRIMARY KEY  (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8');
+/**
+	* Upgrade file for the Online-Store plugin
+	* 
+	* PHP version 5
+	*
+	* @category None
+	* @package  None
+	* @author   Kae Verens <kae@webworks.ie>
+	* @license  GPL 2.0
+	* @link     None
+	*/
+if ($version==0) { // online_store_orders
+	dbQuery(
+					'CREATE TABLE IF NOT EXISTS `online_store_orders` (
+					`id` int(11) NOT NULL auto_increment,
+					`form_vals` text,
+					`invoice` text,
+					`total` float,
+					`date_created` datetime,
+					PRIMARY KEY  (`id`)
+					) ENGINE=MyISAM DEFAULT CHARSET=utf8'
+	);
 	$version=1;
 }
-if($version==1){ // items field
+if ($version==1) { // items field
 	dbQuery('alter table online_store_orders add items text');
 	$version=2;
 }
-if($version==2){ // status
+if ($version==2) { // status
 	dbQuery('alter table online_store_orders add status smallint default 0');
 	$version=3;
 }
-if($version==3){ // currency
+if ($version==3) { // currency
 	$DBVARS['online_store_currency']='EUR';
 	$version=4;
 }
-if($version==4){ // callback
-	// allow a callback to be set in the database to be called when a payment has been completed
+if ($version==4) { // callback
+	/* allow a callback to be set in the database to be called when
+	 *   a payment has been completed
+	 */
 	dbQuery('alter table online_store_orders add callback text');
 	$version=5;
 }
