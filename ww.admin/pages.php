b0485d491d385fdbf561db2ffca798fbea6e01fa
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-07-27 11:11:24 +0000

    remove some unused code

--- ww.admin/pages.php
+++ ww.admin/pages.php
@@ -4,33 +4,21 @@ echo '<h1>'.__('Pages').'</h1>';
 $parent=(int)@$_REQUEST['parent'];
 $msgs='';
 include('pages/pages.funcs.php');
-if(has_access_permissions(ACL_PAGES)){
-	# actions
-	if($action=='delete' || $action==__('Insert Page Details') || $action==__('Update Page Details')){
-		switch($action){
-			case 'delete':                  include('pages/pages.action.delete.php'); break;
-			case __('Insert Page Details'): include('pages/pages.action.new.php');    break;
-			case __('Update Page Details'): include('pages/pages.action.edit.php');   break;
-		}
+if($action=='delete' || $action==__('Insert Page Details') || $action==__('Update Page Details')){
+	switch($action){
+		case 'delete':                  include('pages/pages.action.delete.php'); break;
+		case __('Insert Page Details'): include('pages/pages.action.new.php');    break;
+		case __('Update Page Details'): include('pages/pages.action.edit.php');   break;
 	}
-	$is_an_update=($action==__('Insert Page Details') || $action==__('Update Page Details'));
-	$edit=($is_an_update || $action=='edit')?1:0;
-	if($id&&$edit)setAdminVar('pages_viewing',$id);
-	else if(!$action){
-		$id=getAdminVar('pages_viewing');
-		if(!$id)$id=0;
-		if($id)$edit=1;
-	}
-#	echo '<div style="width:21%;float:left" id="page_menu"></div>';
-	echo '<div class="left-menu">';
-	include 'pages/menu.php';
-	echo '</div>';
-	echo '<div class="has-left-menu">';
-	include('pages/pages.forms.php');
-	echo '</div>';
-}else{
-	echo '<p>'.__('You have no permissions for this page').'</p>';
 }
+$is_an_update=($action==__('Insert Page Details') || $action==__('Update Page Details'));
+$edit=($is_an_update || $action=='edit')?1:0;
+echo '<div class="left-menu">';
+include 'pages/menu.php';
+echo '</div>';
+echo '<div class="has-left-menu">';
+include('pages/pages.forms.php');
+echo '</div>';
 echo '<script>window.page_menu_currentpage='.$id.';</script>',
 		'<script src="/j/jquery.remoteselectoptions.js"></script>',
 		'<style type="text/css">@import "pages/css.css";</style>';
