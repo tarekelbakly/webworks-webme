5154024202f0846e12b42b277eb69db265445590
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-08-06 14:05:44 +0000

    Directory pruner added to kfm. kfm json_encoding the errors so that "\n" can be used for join (it caused me trouble when I accidently pressed upload) Put a link to single view template in the automatically generated multiview template

--- j/kfm/rpc.php
+++ j/kfm/rpc.php
@@ -11,5 +11,29 @@ switch($_REQUEST['action']){
 			exit;
 		}
 		else die('file does not exist');
+	break; // }
+
+
+	case 'prune': // {
+		global $kfm;
+		$root_id = $kfm->setting('root_folder_id');
+		$root_directory = kfmDirectory::getInstance($root_id);
+		prune($root_directory);
+	break; // }
+}
+
+function prune ($dir) {
+	global $root_id;
+	$files = $dir->getFiles();
+	$subDirs = $dir->getSubdirs();
+	// { If the directory contains nothing and is not the root delete it
+	if (!($files&&$subDirs)&&($dir->id!=$root_id)) {
+		return $dir->delete();
+	}
 	// }
+	elseif ($dir->hasSubdirs()) {
+		foreach ($subDirs as $sub) {
+			prune($sub);
+		}
+	}
 }
