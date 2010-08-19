b5207d12f500c19346687271739512269b2c6c1d
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-08-19 13:23:59 +0000

    record generation time in footer of admin (in a comment)

--- trunk/ww.admin/footer.php
+++ trunk/ww.admin/footer.php
@@ -2,6 +2,7 @@
 		</div>
 		<?php
 			echo WW_getScripts();
+			echo '<!-- page genereated in '.(microtime()-$webme_start_time).' seconds -->';
 		?>
 		<script type="text/javascript">var plugins_to_load={<?php echo join(',',$plugins_to_load); ?>};</script>
 	</body>
