f1517b7a7772367a73ab1dd3f917239cf510be2f
kae.verens <kae.verens@gmail.com>
2010-08-31 11:14:13 +0000

    little bit of cleaning up.

--- ww.admin/pages.php
+++ ww.admin/pages.php
@@ -1,14 +1,32 @@
 <?php
+/**
+  * Page section loader. loads the pages menu and the page form iframe
+  *
+  * PHP Version 5
+  *
+  * @category WebworksWebme
+  * @package  None
+  * @author   Kae Verens <kae@webworks.ie>
+  * @license  GPL Version 2
+  * @link     http://www.webworks.ie/
+ */
+
 require 'header.php';
-$id=(int)$_REQUEST['id'];
+if (isset($_REQUEST['id'])) {
+	$id=(int)$_REQUEST['id'];
+}
+else {
+	$id=0;
+}
 echo '<h1>'.__('Pages').'</h1>';
 echo '<div class="left-menu">';
-include 'pages/menu.php';
+require_once 'pages/menu.php';
 echo '</div>';
 echo '<div class="has-left-menu">'
-	.'<iframe id="page-form-wrapper" name="page-form-wrapper" src="/ww.admin/pages/form.php?id='.$id.'"></iframe>'
-	.'</div>';
-echo '<script>window.page_menu_currentpage='.$id.';</script>',
-		'<script src="/j/jquery.remoteselectoptions.js"></script>',
-		'<style type="text/css">@import "pages/css.css";</style>';
+	,'<iframe id="page-form-wrapper" name="page-form-wrapper" '
+	,'src="/ww.admin/pages/form.php?id=',$id,'"></iframe>'
+	,'</div>'
+	,'<script>window.page_menu_currentpage=',$id,';</script>'
+	,'<script src="/j/jquery.remoteselectoptions.js"></script>'
+	,'<style type="text/css">@import "pages/css.css";</style>';
 require 'footer.php';
