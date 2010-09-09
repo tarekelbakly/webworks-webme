1d0ec51c5c14467afb01aadb717a78ef90f43025
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-09 11:54:05 +0000

    little bit of formatting

--- trunk/ww.php_classes/Pages.php
+++ trunk/ww.php_classes/Pages.php
@@ -7,9 +7,9 @@ class Pages{
         global $isadmin;
         $filter=$isadmin?'':' && !(special&2)';
         if (!$byField && is_numeric($v)) $rs=dbAll("select * from pages where parent=$v$filter order by ord,name");
-				else $rs=array();
-				if(!count($rs))$rs=array();
-				foreach($rs as $r)$this->pages[] = Page::getInstance($r['id'],$r);
+		else $rs=array();
+		if(!count($rs))$rs=array();
+		foreach($rs as $r)$this->pages[] = Page::getInstance($r['id'],$r);
         Pages::$instancesByParent[$v] =& $this;
     }
     function getInstancesByParent($pid=0){
@@ -17,19 +17,19 @@ class Pages{
         if (!@array_key_exists($pid,$instancesByParent))new Pages($pid);
         return Pages::$instancesByParent[$pid];
     }
-		function precache($ids){
-			if(count($ids)){
-				$rs3=dbAll('select * from pages where id in ('.join(',',$ids).')');
-				$pvars=dbAll('select * from page_vars where page_id in ('.join(',',$ids).')');
-				$rs2=array();
-				foreach($pvars as $p){
-					if(!isset($rs2[$p['page_id']]))$rs2[$p['page_id']]=array();
-					$rs2[$p['page_id']][]=$p;
-				}
-				foreach($rs3 as $r){
-					if(isset($rs2[$r['id']]))Page::getInstance($r['id'],$r,$rs2[$r['id']]);
-					else Page::getInstance($r['id'],$r);
-				}
+	function precache($ids){
+		if(count($ids)){
+			$rs3=dbAll('select * from pages where id in ('.join(',',$ids).')');
+			$pvars=dbAll('select * from page_vars where page_id in ('.join(',',$ids).')');
+			$rs2=array();
+			foreach($pvars as $p){
+				if(!isset($rs2[$p['page_id']]))$rs2[$p['page_id']]=array();
+				$rs2[$p['page_id']][]=$p;
+			}
+			foreach($rs3 as $r){
+				if(isset($rs2[$r['id']]))Page::getInstance($r['id'],$r,$rs2[$r['id']]);
+				else Page::getInstance($r['id'],$r);
 			}
 		}
+	}
 }
