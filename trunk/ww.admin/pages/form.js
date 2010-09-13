227ba417c7d5c673112fdce17b89590c4c47fff0
kae.verens@gmail.com <kae.verens@gmail.com@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-13 10:48:11 +0000

    you can now use all letters from the "Latin-1 Supplement" in unicode in a page name.

--- trunk/ww.admin/pages/form.js
+++ trunk/ww.admin/pages/form.js
@@ -10,9 +10,9 @@ function pages_validate_name(){
 	var errors=[];
 	if(name.length<4)errors.push('name must be at least 4 characters in length');
 	else{
-		if(/^[^0-9\wáéíóäëiöúàèìì]/.test(name))errors.push('Begins with non-alphanumeric character');
-		if(/[^0-9\wáéíóäëiöúàèìì]$/.test(name))errors.push('Ends with non-alphanumeric character');
-		if(/[^0-9_ -\'\wáéíóäëiöúàèìì]/.test(name))errors.push('Only use alphanumeric characters, spaces, hyphens or underscores');
+		if(/^[^0-9\w\u00C0-\u00FF]/.test(name))errors.push('Begins with non-alphanumeric character');
+		if(/[^0-9\w\u00C0-\u00FF]$/.test(name))errors.push('Ends with non-alphanumeric character');
+		if(/[^0-9_ -\'\w\u00C0-\u00FF]/.test(name))errors.push('Only use alphanumeric characters, spaces, hyphens or underscores');
 	}
 	if(!errors.length){
 		$name[0].className='';
