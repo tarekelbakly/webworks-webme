026d7f2643340ab424e869725caba1b5eb854643
kae.verens@gmail.com <kae.verens@gmail.com@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-22 11:09:28 +0000

    fix "Add News" link

--- ww.plugins/news/admin/display.php
+++ ww.plugins/news/admin/display.php
@@ -10,5 +10,5 @@
 					kae@verens.com
 */
 
-$html.='<p><a href="javascript:;" onclick="pages_new('.$GLOBALS['page']['id'].');">New News Item</a></p>'
+$html.='<p><a href="javascript:;" onclick="window.parent.pages_new('.$GLOBALS['page']['id'].');">New News Item</a></p>'
 	.'<p>To create a new news item simply click on the link above, create a new page with this news page as parent. It will then appear in the frontend as a news item.</p>';
