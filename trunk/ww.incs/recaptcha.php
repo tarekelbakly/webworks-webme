3ad59a50147d949be273e095ef9fc382aca6dc01
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-22 13:38:41 +0000

    Added the recaptcha files

---
+++ trunk/ww.incs/recaptcha.php
@@ -0,0 +1,5 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']
+	.'/ww.incs/recaptcha-php-1.11/recaptchalib.php';
+define('RECAPTCHA_PRIVATE','6LffZAwAAAAAANXRgBLgD51o6fZnvXknLNNXgCUr');
+define('RECAPTCHA_PUBLIC','6LffZAwAAAAAALA70eSDf73p4DTddBu0jgULjukb'); 
