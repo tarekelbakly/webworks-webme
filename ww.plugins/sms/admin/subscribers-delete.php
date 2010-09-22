d53aea7d990e886c44fa5613e0eff75d1a00dc76
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-22 13:34:27 +0000

    A subscriber is deleted from all address books when it is deleted

--- ww.plugins/sms/admin/subscribers-delete.php
+++ ww.plugins/sms/admin/subscribers-delete.php
@@ -1,7 +1,21 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'].'/ww.incs/basics.php';
 if(!is_admin())die('access denied');
-
 $id=(int)$_REQUEST['id'];
+if (!is_int($id)) exit('invalid id');
+
+$addressBooks = dbAll('select id, subscribers from sms_addressbooks');
+foreach($addressBooks as $book) {
+	$subs = $book['subscribers'];
+	$subs = str_replace($id.',', '', $subs);
+	if ($subs==$book['subscribers']) {
+		continue;
+	}
+	dbQuery(
+		'update sms_addressbooks 
+		set subscribers = "'.$subs.'" 
+		where id = '.(int)$book['id']
+	);
+}
 dbQuery('delete from sms_subscribers where id='.$id);
 echo '{"err":0,id:'.$id.'}';
