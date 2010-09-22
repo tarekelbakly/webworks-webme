afaefdd84f8360a9241d200ce8a8898f5580bc4c
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-22 13:56:08 +0000

    Fixed as per notes

--- ww.plugins/sms/admin/subscribers-delete.php
+++ ww.plugins/sms/admin/subscribers-delete.php
@@ -2,7 +2,6 @@
 require $_SERVER['DOCUMENT_ROOT'].'/ww.incs/basics.php';
 if(!is_admin())die('access denied');
 $id=(int)$_REQUEST['id'];
-if (!is_int($id)) exit('invalid id');
 
 $addressBooks = dbAll('select id, subscribers from sms_addressbooks');
 foreach($addressBooks as $book) {
@@ -14,8 +13,8 @@ foreach($addressBooks as $book) {
 	dbQuery(
 		'update sms_addressbooks 
 		set subscribers = "'.$subs.'" 
-		where id = '.(int)$book['id']
+		where id = '.$book['id']
 	);
 }
 dbQuery('delete from sms_subscribers where id='.$id);
-echo '{"err":0,id:'.$id.'}';
+echo '{"err":0,"id:"'.$id.'}';
