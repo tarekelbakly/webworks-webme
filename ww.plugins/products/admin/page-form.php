e19c568dfd92b2256cd5d2b9f5351c59a408af2b
belindah0304 <belindah0304@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-24 13:58:05 +0000

    More tabs converted to jquery

--- ww.plugins/products/admin/page-form.php
+++ ww.plugins/products/admin/page-form.php
@@ -1,26 +1,41 @@
 <?php
 $c.= '<div class="tabs">';
+$c.= '<ul>';
+$c.= '<li><a href="#products-main">Main Details</a></li>';
+$c.= '<li><a href="#products-header">Header</a></li>';
+$c.= '<li><a href="#products-footer">Footer</a></li>';
+$c.= '</ul>';
 // { main details
-$c.= '<div class="tabPage"><h2>Main Details</h2><table class="tab-table">';
+$c.= '<div id="products-main"><table class="tab-table">';
 // { what should be shown
-$c.='<tr><th>What to show</th><td><select id="products_what_to_show" name="page_vars[products_what_to_show]">';
-$arr=array('All products','All products from a specified type','All products from a specified category','A specific product');
-if(!isset($vars['products_what_to_show']))$vars['products_what_to_show']=0;
+$c.= '<tr><th>What to show</th>';
+$c.= '<td><select id="products_what_to_show" name="page_vars[products_what_to_show]">';
+$arr
+	=array(
+		'All products',
+		'All products from a specified type',
+		'All products from a specified category',
+		'A specific product'
+	);
+if (!isset($vars['products_what_to_show']))$vars['products_what_to_show']=0;
 foreach($arr as $k=>$r){
-	$c.='<option value="'.$k.'"';
-	if($k==$vars['products_what_to_show'])$c.=' selected="selected"';
-	$c.='>'.htmlspecialchars($r).'</option>';
+	$c.= '<option value="'.$k.'"';
+	if($k==$vars['products_what_to_show'])$c.= ' selected="selected"';
+	$c.= '>'.htmlspecialchars($r).'</option>';
 }
-$c.='</select></td></tr>';
+$c.= '</select></td></tr>';
 // }
 // { type names
-$c.='<tr id="products_what_to_show_1"><th>Which product type to show</th><td>';
+$c.= '<tr id="products_what_to_show_1"><th>Which product type to show</th><td>';
 $rs=dbAll('select id,name from products_types order by name');
 if($rs===false || !count($rs)){
-	$c.='<p><strong>no types exist.</strong> <a href="/ww.admin/plugin.php?_plugin=products&_page=types">click here to create a product type</a>.</p>';
+	$c.= '<p><strong>no types exist.</strong>'
+		.'<a href="/ww.admin/plugin.php?_plugin=products&_page=types">'
+			.'click here to create a product type</a>.</p>';
 }
 else{
-	$c.='<select name="page_vars[products_type_to_show]"><option value="0"> -- choose -- </option>';
+	$c.='<select name="page_vars[products_type_to_show]"><option value="0">'
+		.'-- choose -- </option>';
 	foreach($rs as $r){
 		$c.='<option value="'.$r['id'].'"';
 		if($r['id']==$vars['products_type_to_show'])$c.=' selected="selected"';
@@ -85,22 +100,25 @@ if(isset($vars['products_order_direction']) && $vars['products_order_direction']
 $c.='>Descending (Z-A)</option></select></td></tr>';
 // }
 // { products per page
-$c.='<tr id="products_per_page"><th>Products per page</th><td><input name="page_vars[products_per_page]" class="small" value="';
-$i=isset($vars['products_per_page'])?(int)$vars['products_per_page']:0;
-if($i<0)$i=0;
-$c.=$i.'" /></td></tr>';
+$c.= '<tr id="products_per_page"><th>Products per page</th><td>';
+$c.= '<input name="page_vars[products_per_page]" class="small" value="';
+$i = isset($vars['products_per_page'])?(int)$vars['products_per_page']:0;
+if ($i<0)$i=0;
+$c.= $i.'" /></td></tr>';
 // }
 $c.= '</table></div>';
 // }
 // { header
-$c.='<div class="tabPage"><h2>Header</h2><p>Text to be shown above the product/product list</p>';
-$c.=ckeditor('body',$page['body']);
-$c.='</div>';
+$c.= '<div id="products-header">';
+$c.= '<p>Text to be shown above the product/product list</p>';
+$c.= ckeditor('body',$page['body']);
+$c.= '</div>';
 // }
 // { footer
-$c.='<div class="tabPage"><h2>Footer</h2><p>Text to be shown below the product/product list</p>';
-$c.=ckeditor('page_vars[footer]',isset($vars['footer'])?$vars['footer']:'');
-$c.='</div>';
+$c.= '<div id="products-footer">';
+$c.= '<p>Text to be shown below the product/product list</p>';
+$c.= ckeditor('page_vars[footer]',isset($vars['footer'])?$vars['footer']:'');
+$c.= '</div>';
 // }
 $c.= '</div>';
-$c.='<script type="text/javascript" src="/ww.plugins/products/admin/page-form.js"></script>';
+$c.= '<script type="text/javascript" src="/ww.plugins/products/admin/page-form.js"></script>';
