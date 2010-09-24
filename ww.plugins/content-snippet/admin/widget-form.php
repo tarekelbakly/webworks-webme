8bc6c1bb601246c29d737660ba0d73d7f7ec6ec7
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-09-24 08:52:09 +0000

    clean up content-snippet HTML before saving it.

--- ww.plugins/content-snippet/admin/widget-form.php
+++ ww.plugins/content-snippet/admin/widget-form.php
@@ -2,6 +2,7 @@
 // var_dump($_REQUEST);
 require $_SERVER['DOCUMENT_ROOT'].'/ww.incs/basics.php';
 if(!is_admin())die('access denied');
+require $_SERVER['DOCUMENT_ROOT'].'/ww.admin/admin_libs.php';
 if(isset($_REQUEST['get_content_snippet'])){
 	$id=(int)$_REQUEST['get_content_snippet'];
 	$r=dbRow('select * from content_snippets where id='.$id);
@@ -17,8 +18,12 @@ if(isset($_REQUEST['get_content_snippet'])){
 if(isset($_REQUEST['action']) && $_REQUEST['action']=='save'){
 	$id=(int)$_REQUEST['id'];
 	$id_was=$id;
-	$html=addslashes(utf8_decode($_REQUEST['html']));
-	$sql="content_snippets set content='$html'";
+	$content=json_decode(utf8_decode($_REQUEST['html']));
+	foreach($content as $k=>$v){
+		$content[$k]->html=sanitise_html($v->html);
+	}
+	$html=json_encode($content);
+	$sql='content_snippets set content="'.addslashes($html).'"';
 	$sql.=',accordion="'.(int)$_REQUEST['accordion'].'"';
 	$sql.=',accordion_direction="'.(int)$_REQUEST['accordion_dir'].'"';
 	$sql.=',images_directory="'.addslashes($_REQUEST['accordion_images']).'"';
