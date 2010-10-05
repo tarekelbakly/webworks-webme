65b482603009a90e2aebef34b87d466892ad39e9
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-10-05 15:50:40 +0000

    optimise banner-images plugin's database usage

--- ww.plugins/banner-image/frontend/banner-image.php
+++ ww.plugins/banner-image/frontend/banner-image.php
@@ -21,14 +21,24 @@ function show_banner($vars){
 		}
 	}
 	else if($GLOBALS['PAGEDATA']->id){
-		$b=dbRow('select * from banners_pages,banners_images where pageid='.$GLOBALS['PAGEDATA']->id.' and bannerid=id order by rand() limit 1');
+		$b=cache_load('banner-images','bypage'.$GLOBALS['PAGEDATA']->id);
+		if ($b===false) {
+			$b=dbAll('select * from banners_pages,banners_images where pageid='.$GLOBALS['PAGEDATA']->id.' and bannerid=id');
+			cache_save('banner-images','bypage'.$GLOBALS['PAGEDATA']->id, $b);
+		}
+		$b=$b[rand(0,count($b)-1)];
 		if($b && count($b) && !$b['html']){
 			$b['html']=banner_image_getImgHTML($b['id']);
 			dbQuery('update banners_pages set html="'.addslashes($b['html']).'" where id='.$b['id']);
 		}
 	}
 	if(!isset($b) || $b===false || !count($b)){
-		$b=dbRow('select * from banners_images where !pages order by rand() limit 1');
+		$b=cache_load('banner-image','all');
+		if ($b===false) {
+			$b=dbAll('select * from banners_images');
+			cache_save('banner-image', 'all', $b);
+		}
+		$b=$b[rand(0,count($b)-1)];
 	}
 	if($b && count($b)){
 		$banner=$b['html'];
