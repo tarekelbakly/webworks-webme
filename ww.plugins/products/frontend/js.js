3f5de94bbc790627233d5401cdb8ef6f3c964c5d
kae.verens@gmail.com <kae.verens@gmail.com@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-10-14 12:30:51 +0000

    fix bug where clicking a thumbnail of a product was changing the main image of /all/ products on a page.

--- ww.plugins/products/frontend/js.js
+++ ww.plugins/products/frontend/js.js
@@ -1,38 +1,43 @@
 $(function(){
 	$('a.products-lightbox').lightBox();
 	$('div.product-images img').click(function(){
-		var src=$('a.products-lightbox img').attr('src');
-		var id=this.src.replace(/.*kfmget\/([0-9]*)[^0-9].*/,'$1');
-		$('a.products-lightbox img').attr('src',src.replace(/kfmget\/([0-9]*)/,'kfmget/'+id));
-		$('a.products-lightbox').attr('href','/kfmget/'+id);
+		var src=$('a.products-lightbox img').attr('src')
+			,id=this.src.replace(/.*kfmget\/([0-9]*)[^0-9].*/,'$1')
+			,$wrapper=$(this).closest('.products-product');
+		$wrapper
+			.find('a.products-lightbox')
+			.attr('href','/kfmget/'+id)
+			.find('img')
+			.attr('src',src.replace(/kfmget\/([0-9]*)/,'kfmget/'+id));
 	});
 	var cache={},lastXhr;
-	$('input[name=products-search]').autocomplete({
-		source: function(request, response){
-			var term = request.term;
-			if ( term in cache ) {
-				response( cache[ term ] );
-				return;
-			}
-			lastXhr = $.getJSON( "/ww.plugins/products/frontend/search.php", request, function( data, status, xhr ) {
-				cache[ term ] = data;
-				if ( xhr === lastXhr ) {
-					response( data );
+	$('input[name=products-search]')
+		.autocomplete({
+			source: function(request, response){
+				var term = request.term;
+				if ( term in cache ) {
+					response( cache[ term ] );
+					return;
 				}
-			});
-		}
-	})
-	.focus(function(){
-		this.value='';
-	})
-	.change(function(){
-		var $this=$(this);
-		var $form=$this.closest('form');
-		if(!$form.length){
-			$form=$this.wrap('<form style="display:inline" method="post" action="'+(document.location.toString())+'" />');
-		}
-		setTimeout(function(){
-			$this.closest('form').submit();
-		},500);
-	});
+				lastXhr = $.getJSON( "/ww.plugins/products/frontend/search.php", request, function( data, status, xhr ) {
+					cache[ term ] = data;
+					if ( xhr === lastXhr ) {
+						response( data );
+					}
+				});
+			}
+		})
+		.focus(function(){
+			this.value='';
+		})
+		.change(function(){
+			var $this=$(this)
+				,$form=$this.closest('form');
+			if(!$form.length){
+				$form=$this.wrap('<form style="display:inline" action="'+(document.location.toString())+'" />');
+			}
+			setTimeout(function(){
+				$this.closest('form').submit();
+			},500);
+		});
 });
