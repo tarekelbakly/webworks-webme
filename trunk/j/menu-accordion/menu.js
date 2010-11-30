4f661018651b8f2e234b842e561dd3523a8cdb8b
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-11-30 16:21:26 +0000

    fix damned stupid mistake

--- trunk/j/menu-accordion/menu.js
+++ trunk/j/menu-accordion/menu.js
@@ -1,13 +1,18 @@
 $(function(){
 	$('div.menu-accordion ul ul').each(function(){
 		var $this=$(this);
-		$this.css('display','none');
-		$this.prev()
-			.addClass('has-submenu')
-			.click(function(){
-				var $this=$(this);
-				$this.next().toggle();
-				return false;
-			});
+		$this
+			.css('display','none')
+			.addClass('is-fg-submenu')
+			.prev()
+				.addClass('has-submenu')
+				.click(function(){
+					var $this=$(this);
+					$this.next().toggle();
+					return false;
+				});
 	});
+	var pid=pagedata.id;
+	var $menu=$('.menu-pid-'+pid).closest('ul');
+	$menu.prev().trigger('click');
 });
