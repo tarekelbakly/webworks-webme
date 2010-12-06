2066f0104767e4decc7a8d31462874b3e4334b62
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2010-12-06 09:12:59 +0000

    favicon is now customisable

--- trunk/ww.admin/siteoptions/general.php
+++ trunk/ww.admin/siteoptions/general.php
@@ -4,6 +4,15 @@ echo '<h2>'.__('General').'</h2>';
 if($action=='Save'){
 	$DBVARS['site_title']=$_REQUEST['site_title'];
 	$DBVARS['site_subtitle']=$_REQUEST['site_subtitle'];
+	if(isset($_FILES['site_favicon']) && file_exists($_FILES['site_favicon']['tmp_name'])){
+		$tmpname=addslashes($_FILES['site_favicon']['tmp_name']);
+		$newdir=USERBASE.'/f/skin_files';
+		mkdir(USERBASE.'/f/skin_files');
+		`rm -fr "$newdir"/favicon-*`;
+		$from=addslashes($_FILES['site_favicon']['tmp_name']);
+		$to=addslashes($newdir.'/favicon.ico');
+		`convert "$from" -resize 32x32 "to"`;
+	}
 	if(isset($_FILES['site_logo']) && file_exists($_FILES['site_logo']['tmp_name'])){
 		$tmpname=addslashes($_FILES['site_logo']['tmp_name']);
 		$newdir=USERBASE.'/f/skin_files';
@@ -29,11 +38,20 @@ if($action=='remove_logo'){
 echo '<form method="post" action="siteoptions.php?page=general" enctype="multipart/form-data"><input type="hidden" name="MAX_FILE_SIZE" value="9999999" /><table>';
 echo '<tr><th>Website Title</th><td><input name="site_title" value="'.htmlspecialchars($DBVARS['site_title']).'" /></td></tr>';
 echo '<tr><th>Website Subtitle</th><td><input name="site_subtitle" value="'.htmlspecialchars($DBVARS['site_subtitle']).'" /></td></tr>';
+// { logo
 echo '<tr><th>Logo</th><td><input type="file" name="site_logo" /><br />';
 if(file_exists(USERBASE.'f/skin_files/logo.png')){
 	echo '<img src="/f/skin_files/logo.png?rand='.mt_rand(0,9999).'" /><a href="/ww.admin/siteoptions.php?action=remove_logo" onclick="return confirm(\'are you sure you want to remove the logo?\')" title="remove logo">[x]</a>';
 }
 echo '</td></tr>';
+// }
+// { favicon
+echo '<tr><th>Favicon</th><td><input type="file" name="site_favicon" /><br />';
+if(file_exists(USERBASE.'f/skin_files/favicon.png')){
+	echo '<img src="/f/skin_files/favicon.png?rand='.mt_rand(0,9999).'" /><a href="/ww.admin/siteoptions.php?action=remove_favicon" onclick="return confirm(\'are you sure you want to remove the favicon?\')" title="remove favicon">[x]</a>';
+}
+echo '</td></tr>';
+// }
 echo '<th>Page Length Limit</th>';
 echo '<td><input type="text" name="site_page_length_limit"';
 if (isset($DBVARS['site_page_length_limit'])) {
