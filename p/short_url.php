4f5fac7976e79b020549d9ccbc48076b432f4373
kae.verens <kae.verens@gmail.com>
2010-12-26 16:34:11 +0000

    move short-url.php and rss.php from /p/ to /ww.incs/

--- p/short_url.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../ww.incs/basics.php';
-
-if(!isset($_GET['s']))exit;
-$s=addslashes($_GET['s']);
-$l=dbOne("select long_url from short_urls where short_url='$s'",'long_url');
-if($l)header('Location: '.$l);
-else echo 'that url is obsolete, or incorrect';
---
+++ ww.incs/short-url.php
@@ -0,0 +1,14 @@
+<?php
+require 'basics.php';
+
+if (!isset($_GET['s'])) {
+	exit;
+}
+$s=addslashes($_GET['s']);
+$l=dbOne("select long_url from short_urls where short_url='$s'", 'long_url');
+if ($l) {
+	header('Location: '.$l);
+}
+else {
+	echo 'that url is obsolete, or incorrect';
+}
