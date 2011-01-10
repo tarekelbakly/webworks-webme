19c5c080c3a3aa04d17e8cbba9fd3a115fc22e25
kae.verens@gmail.com <kae.verens@gmail.com@17faa768-b599-11dd-8610-bdbd52298f7b>
2011-01-10 14:40:12 +0000

    add ability to create a new theme template by copying an existing one.

--- ww.plugins/theme-editor/admin/templates.php
+++ ww.plugins/theme-editor/admin/templates.php
@@ -1,32 +1,54 @@
 <?php
+/**
+	* admin page for editing a template
+	*
+	* PHP Version 5.3
+	*
+	* @category None
+	* @package  None
+	* @author   Kae Verens <kae@kvsites.ie>
+	* @license  GPL Version 2
+	* @link     http://webme.kvsites.ie/
+	**/
 
-if(isset($_REQUEST['action']) && ($_REQUEST['action']=='save')){
-	if (file_put_contents(THEME_DIR.'/'.THEME.'/h/'.$name.'.html',$_REQUEST['theme-body'])===false) {
-		echo '<em>failed to write to '.THEME_DIR.'/'.THEME.'/h/'.$name.'.html. Please check the permissions on the file.</em>';
+$tpl=THEME_DIR.'/'.THEME.'/h/'.$name.'.html';
+
+if (isset($_REQUEST['action']) && ($_REQUEST['action']=='save')) {
+	if (file_put_contents($tpl, $_REQUEST['theme-body'])===false) {
+		echo '<em>failed to write to '.THEME_DIR.'/'.THEME.'/h/'.$name
+			.'.html. Please check the permissions on the file.</em>';
 	}
 }
-$f=file_get_contents(THEME_DIR.'/'.THEME.'/h/'.$name.'.html');
+$f=file_get_contents($tpl);
 
 echo '<form action="/ww.admin/plugin.php" method="post">';
 echo '<input type="hidden" name="_plugin" value="theme-editor" />';
 echo '<input type="hidden" name="_page" value="index" />';
 echo '<input type="hidden" name="name" value="'.$name.'" />';
 echo '<input type="hidden" name="type" value="h" />';
-echo '<textarea id="theme-body" name="theme-body">',htmlspecialchars($f),'</textarea>';
-echo '<br /><input type="submit" onclick="document.getElementById(\'theme-body\').value=editor.getCode();" name="action" value="save" /></form>';
+echo '<textarea id="theme-body" name="theme-body">',htmlspecialchars($f)
+	,'</textarea>';
+echo '<br /><input type="submit" onclick="document.getElementById(\'theme-b'
+	,'ody\').value=editor.getCode();" name="action" value="save" /></form>';
 ?>
 <style>
 .CodeMirror-wrapping{
 	border: 1px solid #000;
 }
 </style>
-<script src="/ww.plugins/theme-editor/j/CodeMirror-0.67/js/codemirror.js"></script>
+<script src="/ww.plugins/theme-editor/j/CodeMirror-0.67/js/codemirror.js">
+</script>
 <script type="text/javascript">
 	var editor = CodeMirror.fromTextArea("theme-body", {
-	  parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
+	  parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js",
+			"parsejavascript.js", "parsehtmlmixed.js"],
 		reindentOnLoad:true,
 		height:450,
 	  path: "/ww.plugins/theme-editor/j/CodeMirror-0.67/js/",
-		stylesheet: ["/ww.plugins/theme-editor/j/CodeMirror-0.67/css/xmlcolors.css", "/ww.plugins/theme-editor/j/CodeMirror-0.67/css/jscolors.css", "/ww.plugins/theme-editor/j/CodeMirror-0.67/css/csscolors.css"]
+		stylesheet: [
+			"/ww.plugins/theme-editor/j/CodeMirror-0.67/css/xmlcolors.css",
+			"/ww.plugins/theme-editor/j/CodeMirror-0.67/css/jscolors.css",
+			"/ww.plugins/theme-editor/j/CodeMirror-0.67/css/csscolors.css"
+		]
 	});
 </script>
