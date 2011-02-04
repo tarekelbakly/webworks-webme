8d965f447a7fea4f9bb80fc2280b632765829e2f
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2011-02-04 16:08:18 +0000

    fix "new news" link

--- ww.plugins/news/admin/display.php
+++ ww.plugins/news/admin/display.php
@@ -10,8 +10,11 @@
 					kae@verens.com
 */
 
-$html.='<p>Click <a href="javascript:;" onclick="window.parent.pages_new('.$page->id.');">here</a> to create a new news item.</p>'
-	.'<p>This page should be displayed in <select name="page_vars[news_type]"><option value="0">headline</option><option value="1"';
+$html='';
+if (isset($page['id'])) {
+	$html.='<p>Click <a href="javascript:;" onclick="window.parent.pages_new('.$page['id'].');">here</a> to create a new news item.</p>';
+}
+$html.='<p>This page should be displayed in <select name="page_vars[news_type]"><option value="0">headline</option><option value="1"';
 if ($vars['news_type']=='1') {
 	$html.=' selected="selected"';
 }
