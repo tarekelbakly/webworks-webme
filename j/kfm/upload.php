0e65ae59ae54b0e3f2cb966ae297219c44e3b2ea
kae.verens <kae.verens@17faa768-b599-11dd-8610-bdbd52298f7b>
2011-02-18 11:32:15 +0000

    minor fixes. if __t is set in the URL, choose the template it names

--- j/kfm/upload.php
+++ j/kfm/upload.php
@@ -112,7 +112,9 @@ if ($kfm->setting('allow_file_upload')) {
 							$data = @exif_read_data($to, 0, true);
 							if (is_array($data)&&isset($data['COMMENT'])&&is_array($data['COMMENT'])) $comment = join("\n", $data['COMMENT']);
 						}
-						$file->setCaption($comment);
+						if (method_exists($file, 'setCaption')) {
+							$file->setCaption($comment);
+						}
 					}
 					else if (isset($_POST['kfm_unzipWhenUploaded'])&&$_POST['kfm_unzipWhenUploaded']) {
 						kfm_extractZippedFile($fid);
